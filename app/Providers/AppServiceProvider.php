ea34a5ccb63080389f05ae05d0138fdd0710cd39
Matej Arh <matej.arh@gmail.com>
2024-04-24 09:42:46 +0200

    sanitazion

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,11 @@
 namespace App\Providers;
 
 use Illuminate\Auth\Events\Login;
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Facades\RateLimiter;
+
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -22,9 +26,19 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Event::listen(
-            Login::class ,
+            Login::class,
             \App\Listeners\RecordUserLogin::class,
 
         );
+
+        RateLimiter::for('global', function (Request $request) {
+            return Limit::perMinute(1000)->response(function (Request $request, array $headers) {
+                return response(__('Too Many Requests'), 429, $headers);
+            });
+        });
+
+        RateLimiter::for('admins', function (Request $request) {
+            return Limit::perMinute(120)->by($request->session()->get('login.id'));
+        });
     }
 }
