271d8ae5557f604fec4813c93703c4fc8c57dfbd
Matej Arh <matej.arh@gmail.com>
2024-04-25 13:56:00 +0200

    initial

---
+++ app/Listeners/RecordUserLogout.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Listeners;
+
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use Laravel\Jetstream\Agent;
+use Stevebauman\Location\Facades\Location;
+
+class RecordUserLogout
+{
+    /**
+     * Create the event listener.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     */
+    public function handle(object $event): void
+    {
+        $agent = $this->createAgent(request()->header('user-agent'));
+
+        \Log::debug('event', [request()->ip()]);
+        $ip=request()->ip();
+        // $ip = "84.52.175.124";
+        $event->user->activities()->create([
+
+                'user_id' => auth()->id(),
+                'type' => 'created_logout',
+                'subject_id' => auth()->id(),
+                'subject_type' => 'App\\Models\\User',
+                'agent' => [
+                    'is_desktop' => $agent->isDesktop(),
+                    'is_tablet' => $agent->isTablet(),
+                    'is_mobile' => $agent->isMobile(),
+                    'platform' => $agent->platform(),
+                    'browser' => $agent->browser(),
+                ],
+                'location' => cache()->rememberForever('location-' . $ip, function () use ($ip) {
+                    return Location::get($ip);
+                }),
+
+        ]);
+
+    }
+
+    protected function createAgent($session)
+    {
+        return tap(new Agent(), fn($agent) => $agent->setUserAgent($session));
+    }
+}
