635c48f8b500cb94a701bd192a760ea7a49a12a4
Matej Arh <matej.arh@gmail.com>
2024-04-27 09:53:19 +0200

    left and bottom aligment

--- resources/js/Components/Tooltip.vue
+++ resources/js/Components/Tooltip.vue
@@ -11,17 +11,17 @@ const props = defineProps({
 
 const aligmentClasses = computed(() => {
     return {
-        'top': '-top-12 ',
-        'bottom': '-bottom-12 ',
-        'left': '-left-12',
+        'top': '-top-12 left-1/2 -translate-x-1/2',
+        'bottom': '-bottom-12 left-1/2 -translate-x-1/2',
+        'left': 'right-[120%] -top-2',
         'right': '-right-12',
     }[props.location];
 })
 const arrowAligmentClasses = computed(() => {
     return {
-        'top': '-bottom-1',
-        'bottom': '-top-1',
-        'left': '-right-1',
+        'top': '-bottom-1 left-1/2 -translate-x-1/2',
+        'bottom': '-top-1 left-1/2 -translate-x-1/2',
+        'left': '-right-1 bottom-1/2 translate-y-1/2',
         'right': '-left-1',
     }[props.location];
 })
@@ -34,13 +34,13 @@ const show = ref(false)
             <ApearDisapearFadeTransition>
                 <div v-show="show"
                     :class="aligmentClasses"
-                    class="inline-block absolute whitespace-nowrap left-1/2 transform -translate-x-1/2 z-50 py-2 px-3 text-sm font-medium text-white bg-gradient-to-t from-black  to-gray-950 rounded-lg shadow-sm transition-opacity duration-300"
+                    class="inline-block absolute whitespace-nowrap  transform z-50 py-2 px-3 text-sm font-medium text-white bg-gradient-to-t from-black  to-gray-950 rounded-lg shadow-sm transition-opacity duration-300"
                     >
                     <div class="z-10">
                         {{ text }}
 
                     </div>
-                    <div :class="arrowAligmentClasses" class="absolute w-3 z-0 h-3 rotate-45 bg-black left-1/2 -translate-x-1/2  text-lg" />
+                    <div :class="arrowAligmentClasses" class="absolute w-3 z-0 h-3 rotate-45 bg-black transform  text-lg" />
                 </div>
 
             </ApearDisapearFadeTransition>
