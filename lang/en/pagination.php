e578851079c7650015f244e80cf975d2b294ab18
Matej Arh <matej.arh@gmail.com>
2024-04-30 21:35:47 +0200

    adding "page"

--- lang/en/pagination.php
+++ lang/en/pagination.php
@@ -5,4 +5,5 @@ declare(strict_types=1);
 return [
     'next'     => 'Next &raquo;',
     'previous' => '&laquo; Previous',
+    'page'     => 'page',
 ];
--- lang/sl/pagination.php
+++ lang/sl/pagination.php
@@ -5,4 +5,5 @@ declare(strict_types=1);
 return [
     'next'     => 'Naslednja &raquo;',
     'previous' => '&laquo; Prejšnja',
+    'page'     => 'stran',
 ];
