8a6381c4d01089a87451f6d4df69c94510b68545
Matej Arh <matej.arh@gmail.com>
2024-04-30 21:48:43 +0200

    splited code

--- resources/js/Pages/Profile/Partials/TwoFactorAuthenticationForm.vue
+++ resources/js/Pages/Profile/Partials/TwoFactorAuthenticationForm.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, computed, watch } from 'vue';
+import { ref, computed, watch, watchEffect } from 'vue';
 import { router, useForm, usePage } from '@inertiajs/vue3';
 import ActionSection from '@/Components/ActionSection.vue';
 import ConfirmsPassword from '@/Components/ConfirmsPassword.vue';
@@ -9,6 +9,8 @@ import InputLabel from '@/Components/InputLabel.vue';
 import PrimaryButton from '@/Components/PrimaryButton.vue';
 import SecondaryButton from '@/Components/SecondaryButton.vue';
 import TextInput from '@/Components/TextInput.vue';
+import Tooltip from '@/Components/Tooltip.vue';
+import GALogo from '/resources/images/branding/google_authenticator-logo.png'
 
 const props = defineProps({
     requiresConfirmation: Boolean,
@@ -26,16 +28,31 @@ const confirmationForm = useForm({
     code: '',
 });
 
+const codeSplited = ref([])
+
+const code1 = ref(null)
+
 const twoFactorEnabled = computed(
-    () => ! enabling.value && page.props.auth.user?.two_factor_enabled,
+    () => !enabling.value && page.props.auth.user?.two_factor_enabled,
 );
 
 watch(twoFactorEnabled, () => {
-    if (! twoFactorEnabled.value) {
+    if (!twoFactorEnabled.value) {
         confirmationForm.reset();
         confirmationForm.clearErrors();
+        // codeSplited.value = [];
+        // code1.value.focus()
     }
 });
+
+watchEffect(() => {
+    if (codeSplited.value.length >= 6) {
+        let code = codeSplited.value.join(',').replaceAll(',', '')
+        // console.log(code)
+        confirmationForm.code = code
+        confirmTwoFactorAuthentication()
+    }
+})
 
 const enableTwoFactorAuthentication = () => {
     enabling.value = true;
@@ -73,6 +90,7 @@ const showRecoveryCodes = () => {
 };
 
 const confirmTwoFactorAuthentication = () => {
+    confirming.value = true;
     confirmationForm.post(route('two-factor.confirm'), {
         errorBag: "confirmTwoFactorAuthentication",
         preserveScroll: true,
@@ -82,6 +100,14 @@ const confirmTwoFactorAuthentication = () => {
             qrCode.value = null;
             setupKey.value = null;
         },
+        onFinish: () => {
+            codeSplited.value = [];
+            confirmationForm.reset()
+        },
+        onError: () => {
+            code1.value.focus()
+
+        }
     });
 };
 
@@ -107,44 +133,55 @@ const disableTwoFactorAuthentication = () => {
 <template>
     <ActionSection>
         <template #title>
-            {{__('Two Factor Authentication')}}
+            {{ __('Two Factor Authentication') }}
         </template>
 
         <template #description>
-            {{__('Add additional security to your account using two factor authentication.')}}
+            {{ __('Add additional security to your account using two factor authentication.') }}
         </template>
 
         <template #content>
-            <h3 v-if="twoFactorEnabled && ! confirming" class="text-lg font-medium text-gray-900 dark:text-gray-100">
-                {{__('You have enabled two factor authentication.')}}
+            <h3 v-if="twoFactorEnabled && !confirming" class="text-lg font-medium text-gray-900 dark:text-gray-100">
+                {{ __('You have enabled two factor authentication.') }}
             </h3>
 
             <h3 v-else-if="twoFactorEnabled && confirming" class="text-lg font-medium text-gray-900 dark:text-gray-100">
-                {{__('Finish enabling two factor authentication.')}}
+                {{ __('Finish enabling two factor authentication.') }}
             </h3>
 
             <h3 v-else class="text-lg font-medium text-gray-900 dark:text-gray-100">
-                {{__('You have not enabled two factor authentication.')}}
+                {{ __('You have not enabled two factor authentication.') }}
             </h3>
 
             <div class="mt-3 max-w-xl text-sm text-gray-600 dark:text-gray-200">
                 <p>
-                    {{__('When two factor authentication is enabled, you will be prompted for a secure, random token during authentication. You may retrieve this token from your phone\'s Google Authenticator application.')}}
+                    {{ __('When two factor authentication is enabled, you will be prompted for a secure, random token during authentication. You may retrieve this token from your phone\'s Google Authenticator application.')}}
                 </p>
-                <a target="_blank" href="https://play.google.com/store/apps/details?id=com.google.android.apps.authenticator2&pli=1">
-                    {{__('Download Google Authenticator')}}
-                </a>
+
+                <div class="flex mt-4">
+                    <Tooltip :text="__('Download Google Authenticator')">
+                        <div class="mx-auto text-center">
+
+                            <a target="_blank" class="relative text-primary-600"
+                                href="https://play.google.com/store/apps/details?id=com.google.android.apps.authenticator2&pli=1">
+                                    <img :src="GALogo" class="w-16 h-16 mx-auto hover:rotate-180 transform ease-in-out duration-300">
+                                    {{ __('Download Google Authenticator') }}
+                                </a>
+                        </div>
+                    </Tooltip>
+
+                </div>
             </div>
 
             <div v-if="twoFactorEnabled">
                 <div v-if="qrCode">
                     <div class="mt-4 max-w-xl text-sm text-gray-600">
                         <p v-if="confirming" class="font-semibold">
-                            {{__('To finish enabling two factor authentication, scan the following QR code using your phone\'s authenticator application or enter the setup key and provide the generated OTP code.')}}
+                            {{ __('To finish enabling two factor authentication, scan the following QR code using your phone\'s authenticator application or enter the setup key and provide the generated OTP code.')}}
                         </p>
 
                         <p v-else>
-                            {{__('Two factor authentication is now enabled. Scan the following QR code using your phone\'s authenticator application or enter the setup key.')}}
+                            {{ __('Two factor authentication is now enabled. Scan the following QR code using your phone\'s authenticator application or enter the setup key.')}}
                         </p>
                     </div>
 
@@ -152,38 +189,73 @@ const disableTwoFactorAuthentication = () => {
 
                     <div v-if="setupKey" class="mt-4 max-w-xl text-sm text-gray-600">
                         <p class="font-semibold">
-                            {{__('Setup Key')}}: <span v-html="setupKey"></span>
+                            {{ __('Setup Key') }}: <span v-html="setupKey"></span>
                         </p>
                     </div>
 
                     <div v-if="confirming" class="mt-4">
                         <InputLabel for="code" :value="__('Code')" />
 
-                        <TextInput
-                            id="code"
-                            v-model="confirmationForm.code"
-                            type="text"
-                            name="code"
-                            class="block mt-1 w-1/2"
-                            inputmode="numeric"
-                            autofocus
-                            autocomplete="one-time-code"
-                            :has-error="!!confirmationForm.errors.code"
-                            @keyup.enter="confirmTwoFactorAuthentication"
-                        />
+                        <div class="flex mb-2 space-x-2 rtl:space-x-reverse">
+                            <!-- @keyup.enter="confirmTwoFactorAuthentication" -->
+                            <TextInput id="code-1" v-model="codeSplited[0]" type="text" name="code-1"
+                                ref="code1"
+                                pattern="[0-9]{1,1}"
+                                class="block w-9 h-9 py-3 text-base font-extrabold text-center text-gray-900 bg-white border border-gray-300 rounded-lg focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                                inputmode="numeric" autofocus :has-error="!!confirmationForm.errors.code"
+                                @input="Number.isInteger(parseInt($event.data)) ? $refs.code2.focus() : codeSplited.splice(0, 1)"
+                                @keyup.backspace="codeSplited.splice(0, 1)" />
+                            <TextInput id="code-2" v-model="codeSplited[1]" type="text" name="code-2"
+                                ref="code2"
+                                pattern="[0-9]{1,1}"
+                                class="block w-9 h-9 py-3 text-base font-extrabold text-center text-gray-900 bg-white border border-gray-300 rounded-lg focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                                inputmode="numeric" :has-error="!!confirmationForm.errors.code"
+                                @input="Number.isInteger(parseInt($event.data)) ? $refs.code3.focus() : codeSplited.splice(1, 1)"
+                                @keyup.backspace="codeSplited.splice(1, 1), $refs.code1.focus()" />
+                            <TextInput id="code-3" v-model="codeSplited[2]" type="text" name="code-3"
+                                ref="code3"
+                                pattern="[0-9]{1,1}"
+                                class="block w-9 h-9 py-3 text-base font-extrabold text-center text-gray-900 bg-white border border-gray-300 rounded-lg focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                                inputmode="numeric" :has-error="!!confirmationForm.errors.code"
+                                @input="Number.isInteger(parseInt($event.data)) ? $refs.code4.focus() : codeSplited.splice(2, 1)"
+                                @keyup.backspace="codeSplited.splice(2, 1), $refs.code2.focus()" />
+                            <TextInput id="code-4" v-model="codeSplited[3]" type="text" name="code-4"
+                                ref="code4"
+                                pattern="[0-9]{1,1}"
+                                class="block w-9 h-9 py-3 text-base font-extrabold text-center text-gray-900 bg-white border border-gray-300 rounded-lg focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                                inputmode="numeric" :has-error="!!confirmationForm.errors.code"
+                                @input="Number.isInteger(parseInt($event.data)) ? $refs.code5.focus() : codeSplited.splice(3, 1)"
+                                @keyup.backspace="codeSplited.splice(3, 1), $refs.code3.focus()" />
+                            <TextInput id="code-5" v-model="codeSplited[4]" type="text" name="code-5"
+                                ref="code5"
+                                pattern="[0-9]{1,1}"
+                                class="block w-9 h-9 py-3 text-base font-extrabold text-center text-gray-900 bg-white border border-gray-300 rounded-lg focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                                inputmode="numeric" :has-error="!!confirmationForm.errors.code"
+                                @input="Number.isInteger(parseInt($event.data)) ? $refs.code6.focus() : codeSplited.splice(4, 1)"
+                                @keyup.backspace="codeSplited.splice(4, 1), $refs.code4.focus()" />
+                            <TextInput id="code-6" v-model="codeSplited[5]" type="text" name="code-6"
+                                ref="code6"
+                                pattern="[0-9]{1,1}"
+                                class="block w-9 h-9 py-3 text-base font-extrabold text-center text-gray-900 bg-white border border-gray-300 rounded-lg focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                                inputmode="numeric" :has-error="!!confirmationForm.errors.code"
+                                @change="Number.isInteger(parseInt($event.data)) ? confirmTwoFactorAuthentication : codeSplited.splice(5, 1)"
+                                @keyup.enter="confirmTwoFactorAuthentication"
+                                @keyup.backspace="codeSplited.splice(5, 1), $refs.code5.focus()" />
+
+                        </div>
 
                         <InputError :message="confirmationForm.errors.code" class="mt-2" />
                     </div>
                 </div>
 
-                <div v-if="recoveryCodes.length > 0 && ! confirming">
-                    <div class="mt-4 max-w-xl text-sm text-gray-600">
+                <div v-if="recoveryCodes.length > 0 && !confirming">
+                    <div class="mt-4 max-w-xl text-sm text-gray-600 dark:text-gray-300">
                         <p class="font-semibold">
-                            {{__('Store these recovery codes in a secure password manager. They can be used to recover access to your account if your two factor authentication device is lost.')}}
+                            {{ __('Store these recovery codes in a secure password manager. They can be used to recover access to your account if your two factor authentication device is lost.')}}
                         </p>
                     </div>
 
-                    <div class="grid gap-1 max-w-xl mt-4 px-4 py-4 font-mono text-sm bg-gray-100 rounded-lg">
+                    <div class="grid gap-1 max-w-xl mt-4 px-4 py-4 font-mono text-sm bg-gray-100 dark:bg-gray-950 rounded-lg">
                         <div v-for="code in recoveryCodes" :key="code">
                             {{ code }}
                         </div>
@@ -192,62 +264,43 @@ const disableTwoFactorAuthentication = () => {
             </div>
 
             <div class="mt-5">
-                <div v-if="! twoFactorEnabled">
+                <div v-if="!twoFactorEnabled">
                     <ConfirmsPassword @confirmed="enableTwoFactorAuthentication">
                         <PrimaryButton type="button" :class="{ 'opacity-25': enabling }" :disabled="enabling">
-                            {{__('Enable')}}
+                            {{ __('Enable') }}
                         </PrimaryButton>
                     </ConfirmsPassword>
                 </div>
 
                 <div v-else>
                     <ConfirmsPassword @confirmed="confirmTwoFactorAuthentication">
-                        <PrimaryButton
-                            v-if="confirming"
-                            type="button"
-                            class="me-3"
-                            :class="{ 'opacity-25': enabling }"
-                            :disabled="enabling"
-                        >
-                            {{__('Confirm')}}
+                        <PrimaryButton v-if="confirming" type="button" class="me-3" :class="{ 'opacity-25': enabling }"
+                            :disabled="enabling">
+                            {{ __('Confirm') }}
                         </PrimaryButton>
                     </ConfirmsPassword>
 
                     <ConfirmsPassword @confirmed="regenerateRecoveryCodes">
-                        <SecondaryButton
-                            v-if="recoveryCodes.length > 0 && ! confirming"
-                            class="me-3"
-                        >
-                            {{__('Regenerate Recovery Codes')}}
+                        <SecondaryButton v-if="recoveryCodes.length > 0 && !confirming" class="me-3">
+                            {{ __('Regenerate Recovery Codes') }}
                         </SecondaryButton>
                     </ConfirmsPassword>
 
                     <ConfirmsPassword @confirmed="showRecoveryCodes">
-                        <SecondaryButton
-                            v-if="recoveryCodes.length === 0 && ! confirming"
-                            class="me-3"
-                        >
-                            {{__('Show Recovery Codes')}}
+                        <SecondaryButton v-if="recoveryCodes.length === 0 && !confirming" class="me-3">
+                            {{ __('Show Recovery Codes') }}
                         </SecondaryButton>
                     </ConfirmsPassword>
 
                     <ConfirmsPassword @confirmed="disableTwoFactorAuthentication">
-                        <SecondaryButton
-                            v-if="confirming"
-                            :class="{ 'opacity-25': disabling }"
-                            :disabled="disabling"
-                        >
-                            {{__('Cancel')}}
+                        <SecondaryButton v-if="confirming" :class="{ 'opacity-25': disabling }" :disabled="disabling">
+                            {{ __('Cancel') }}
                         </SecondaryButton>
                     </ConfirmsPassword>
 
                     <ConfirmsPassword @confirmed="disableTwoFactorAuthentication">
-                        <DangerButton
-                            v-if="! confirming"
-                            :class="{ 'opacity-25': disabling }"
-                            :disabled="disabling"
-                        >
-                            {{__('Disable')}}
+                        <DangerButton v-if="!confirming" :class="{ 'opacity-25': disabling }" :disabled="disabling">
+                            {{ __('Disable') }}
                         </DangerButton>
                     </ConfirmsPassword>
                 </div>
