e8b8432720166732190681108536b195a502cf74
Matej Arh <matej.arh@gmail.com>
2024-04-30 22:07:19 +0200

    properties initial

---
+++ app/Models/Property.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\Pivot;
+
+class Property extends Pivot
+{
+    use HasFactory;
+
+    public function owner() : BelongsTo {
+        return $this->belongsTo(User::class, 'owner_id');
+    }
+}
