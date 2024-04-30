ab3c38cf3ba60e6968707345777ff2c196ed731a
Matej Arh <matej.arh@gmail.com>
2024-04-30 21:48:58 +0200

    new design

--- resources/js/Components/DialogModal.vue
+++ resources/js/Components/DialogModal.vue
@@ -24,24 +24,39 @@ const close = () => {
 </script>
 
 <template>
-    <Modal
-        :show="show"
-        :max-width="maxWidth"
-        :closeable="closeable"
-        @close="close"
-    >
-        <div class="px-6 py-4">
-            <div class="text-lg font-medium text-gray-900 dark:text-gray-50">
-                <slot name="title" />
+    <Modal :show="show" :max-width="maxWidth" :closeable="closeable" @close="close">
+
+        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+
+            <!-- Modal header -->
+            <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+                <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
+                    <slot name="title" />
+                </h3>
+                <button type="button"
+                @click="close"
+                    class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                    data-modal-toggle="crud-modal">
+                    <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                        viewBox="0 0 14 14">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                    </svg>
+                    <span class="sr-only">{{__('Close modal')}}</span>
+                </button>
             </div>
 
-            <div class="mt-4 text-sm text-gray-600 dark:text-gray-50">
+        </div>
+
+        <div class="p-4 md:p-5">
+            <div class="mb-4">
                 <slot name="content" />
+
             </div>
+
+
+            <slot name="footer"  />
         </div>
 
-        <div class="flex flex-row justify-end px-6 py-4 bg-gray-100 dark:bg-gray-900 text-end">
-            <slot name="footer" />
-        </div>
     </Modal>
 </template>
