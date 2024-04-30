375466773dc56f4065261b625fad39273921f148
Matej Arh <matej.arh@gmail.com>
2024-04-30 21:49:09 +0200

    colors

--- resources/js/Components/PrimaryButton.vue
+++ resources/js/Components/PrimaryButton.vue
@@ -9,7 +9,7 @@ defineProps({
 
 <template>
     <button :type="type"
-        class="text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-600 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-800 dark:focus:ring-primary-800">
+        class="transition-all duration-150 ease-out text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-600 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-800 dark:focus:ring-primary-800">
         <slot />
     </button>
 
