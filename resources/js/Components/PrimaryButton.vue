825c1bf3ef68aa7177a4d48e0af49c99f9287f22
Matej Arh <matej.arh@gmail.com>
2024-05-02 21:42:46 +0200

    scale on hover and active

--- resources/js/Components/PrimaryButton.vue
+++ resources/js/Components/PrimaryButton.vue
@@ -9,7 +9,7 @@ defineProps({
 
 <template>
     <button :type="type"
-        class="transition-all duration-150 ease-out text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-600 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-800 dark:focus:ring-primary-800">
+        class="hover:scale-105 active:scale-95 transition-all duration-150 ease-out text-white bg-primary-600 hover:bg-primary-700 focus:ring-0 focus:outline-none  font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-800 shadow-lg">
         <slot />
     </button>
 
--- resources/js/Components/ScrollToTop.vue
+++ resources/js/Components/ScrollToTop.vue
@@ -21,7 +21,7 @@ const scrollToTop = () => {
                 <Tooltip :text="__('Scroll To Top')">
 
                     <button @click="scrollToTop"
-                        class="inline-flex bg-opacity-50 dark:bg-opacity-50 hover:bg-opacity-70 hover:dark:bg-opacity-70 justify-center p-0 text-gray-100 rounded cursor-pointer dark:text-gray-100 dark:hover:text-white hover:text-gray-50 bg-primary-600 dark:bg-gray-600 transition ease-in-out duration-300 hover:-translate-y-1">
+                        class="hover:scale-105 active:scale-95 inline-flex bg-opacity-50 dark:bg-opacity-50 hover:bg-opacity-70 hover:dark:bg-opacity-70 justify-center p-0 text-gray-100 rounded cursor-pointer dark:text-gray-100 dark:hover:text-white hover:text-gray-50 bg-primary-600 dark:bg-gray-600 transition ease-in-out duration-300 hover:-translate-y-1">
                         <ArrowUpIcon class="w-12 h-12" />
                     </button>
                 </Tooltip>
