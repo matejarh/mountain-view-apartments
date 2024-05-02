6b850220e4b7ca2fc3a464fee2db5af24757c243
Matej Arh <matej.arh@gmail.com>
2024-05-02 22:22:51 +0200

    initial

---
+++ resources/js/Pages/Admin/Galleries/Show.vue
@@ -0,0 +1,23 @@
+<script setup>
+import ActionSection from '@/Components/ActionSection.vue';
+import FullLayout from '@/Layouts/FullLayout.vue';
+
+defineProps({
+    gallery: Object
+})
+
+</script>
+
+<template>
+    <FullLayout :title="__('Gallery')">
+        <ActionSection :fullwidth="true">
+            <template #title>
+                {{ __(gallery.name) }}
+            </template>
+            <template #content>
+                {{ gallery }}
+            </template>
+
+        </ActionSection>
+    </FullLayout>
+</template>
