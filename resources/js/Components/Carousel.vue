c3e16af1005c340ef745309d42e0ac523b0da36b
Matej Arh <matej.arh@gmail.com>
2024-05-03 10:16:06 +0200

    image_path > photo_url

--- resources/js/Components/Carousel.vue
+++ resources/js/Components/Carousel.vue
@@ -1,26 +1,42 @@
 <script setup>
 import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel'
 import '/resources/css/carousel.css'
+import { onMounted, ref } from 'vue';
 
 defineProps({
     items: Array,
 })
+
+const i18n = ref({
+    ariaNextSlide: "Navigate to next slide",
+    ariaPreviousSlide: "Navigate to previous slide",
+    ariaNavigateToSlide: "Navigate to slide {slideNumber}",
+    ariaGallery: "Gallery",
+    itemXofY: "Item {currentSlide} of {slidesCount}",
+    iconArrowUp: "Arrow pointing upwards",
+    iconArrowDown: "Arrow pointing downwards",
+    iconArrowRight: "Arrow pointing to the right",
+    iconArrowLeft: "Arrow pointing to the left",
+})
+
 </script>
 
 <template>
     <div id="default-carousel" class="relative w-full h-52 md:h-56" data-carousel="slide">
-        <carousel :items-to-show="1" :autoplay="2000" :wrap-around="true" :transition="600" :pause-autoplay-on-hover="true" class="relative  overflow-hidden rounded-t-lg ">
+        <carousel :items-to-show="1" :autoplay="2000" :wrap-around="true" :transition="600"
+            :i18n="i18n"
+            :pause-autoplay-on-hover="true" class="relative  overflow-hidden rounded-t-lg ">
             <slide v-for="slide, key in items" :key="key" class="">
-                <div class="h-48 md:h-52">
+                <div class="h-52 md:h-56">
 
-                    <img :src="slide.image_path"
-                        class="absolute block h-auto  -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
+                    <img :src="slide.photo_url"
+                        class="absolute block h-full w-auto  -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
                 </div>
             </slide>
 
             <template #addons="{ slidesCount }">
                 <navigation v-if="slidesCount > 1" />
-                <pagination v-if="slidesCount > 1"/>
+                <pagination v-if="slidesCount > 1" />
             </template>
         </carousel>
 
