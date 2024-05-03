5edb8f05fa2c4b33309fe94a54d242acc4626fdd
Matej Arh <matej.arh@gmail.com>
2024-05-03 10:17:50 +0200

    attach images dialog

--- resources/js/Pages/Admin/Galleries/Show.vue
+++ resources/js/Pages/Admin/Galleries/Show.vue
@@ -1,23 +1,53 @@
 <script setup>
 import ActionSection from '@/Components/ActionSection.vue';
 import FullLayout from '@/Layouts/FullLayout.vue';
+import ImageCard from './_partials/ImageCard.vue';
+import GridSection from '@/Components/GridSection.vue';
+import Tooltip from '@/Components/Tooltip.vue';
+import CirclePlusIcon from '@/Icons/CirclePlusIcon.vue';
+import ImageIcon from '@/Icons/ImageIcon.vue';
+import { ref } from 'vue';
+import AttachImagesDialog from './_partials/AttachImagesDialog.vue';
 
-defineProps({
-    gallery: Object
-})
-
+const showImagesDialog = ref(false)
 </script>
 
 <template>
     <FullLayout :title="__('Gallery')">
         <ActionSection :fullwidth="true">
             <template #title>
-                {{ __(gallery.name) }}
+                {{ __($page.props.gallery.name) }}
+            </template>
+            <template #description>
+                {{ __($page.props.gallery.description) }}
             </template>
             <template #content>
-                {{ gallery }}
+                <GridSection v-if="$page.props.gallery.images.length > 0">
+
+                    <ImageCard v-for="image, key in $page.props.gallery.images" :key="image.id" :image="image" />
+                </GridSection>
+
+                <div v-else class="">
+                    <div class="w-full text-center cursor-pointer" @click="showImagesDialog = true">
+                        <ImageIcon class="text-gray-300 dark:text-gray-400 w-32 h-32 mx-auto" />
+
+                        <p class="text-gray-300 dark:text-gray-400 font-semibold ">{{ __('No images yet!') }}</p>
+                        <p class="text-gray-300 dark:text-gray-400 text-sm">{{__('Click here to add some')}}</p>
+                    </div>
+                </div>
+
             </template>
 
         </ActionSection>
+        <div class=" fixed top-28 sm:top-20 right-4 md:top-20  md:right-4 z-30">
+            <Tooltip :text="__('Add New Gallery')" location="bottom">
+                <button @click="showImagesDialog = true"
+                    class=" drop-shadow-lg rounded-full bg-transparent ">
+                    <CirclePlusIcon
+                        class="w-16 h-16 text-amazon-400 hover:scale-105 hover:rotate-180 active:scale-95 transition " />
+                </button>
+            </Tooltip>
+        </div>
+        <AttachImagesDialog :show="showImagesDialog" @close="showImagesDialog = false" :gallery="$page.props.gallery" />
     </FullLayout>
 </template>
