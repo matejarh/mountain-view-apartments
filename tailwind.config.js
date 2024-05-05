8b8ff70b7103f58ac44d312b8c4eb351f9b75cc9
Matej Arh <matej.arh@gmail.com>
2024-05-05 11:54:13 +0200

    shake animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,6 +20,18 @@ export default {
             backgroundImage: {
                 'image-bled': "url('/resources/images/errors/background.jpg')",
             },
+            animation: {
+                shake: 'shake 0.82s cubic-bezier(0.36, 0.07, 0.19, 0.97) both',
+            },
+            keyframes: {
+                shake: {
+                    '10%, 90%': { transform: 'translate3d(-1px, 0, 0)' },
+                    '20%,80%': { transform: 'translate3d(2px, 0, 0)' },
+                    '30%, 50%, 70%': { transform: 'translate3d(-4px, 0, 0)' },
+                    '40%, 60%': { transform: 'translate3d(4px, 0, 0)' },
+                },
+
+            },
             colors: {
                 'primary': {
                     '50': '#eafeff',
@@ -33,8 +45,8 @@ export default {
                     '800': '#0b5d7f',
                     '900': '#0e4e6b',
                     '950': '#02334a',
-                  },
-                  'amazon': {
+                },
+                'amazon': {
                     '50': '#f1f8f3',
                     '100': '#ddeee1',
                     '200': '#bdddc7',
@@ -46,8 +58,8 @@ export default {
                     '800': '#204533',
                     '900': '#1b392a',
                     '950': '#0e2018',
-                  },
-                  'gray': {
+                },
+                'gray': {
                     '50': '#f6f6f6',
                     '100': '#e7e7e7',
                     '200': '#d1d1d1',
@@ -59,8 +71,8 @@ export default {
                     '800': '#454545',
                     '900': '#3d3d3d',
                     '950': '#262626',
-                  },
-                  'bittersweet': {
+                },
+                'bittersweet': {
                     '50': '#fff2f1',
                     '100': '#ffe4e1',
                     '200': '#ffccc7',
@@ -72,7 +84,7 @@ export default {
                     '800': '#a02014',
                     '900': '#842218',
                     '950': '#480d07',
-                  },
+                },
             },
         },
     },
