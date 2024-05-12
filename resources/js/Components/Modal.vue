9c3a488ff1530223c7e04c9a02a60af50fb47452
Matej Arh <matej.arh@gmail.com>
2024-05-12 12:37:16 +0200

    teleport to body

--- resources/js/Components/Modal.vue
+++ resources/js/Components/Modal.vue
@@ -73,51 +73,53 @@ const maxWidthClass = computed(() => {
 </script>
 
 <template>
-    <dialog class="z-50 m-0 min-h-full min-w-full bg-transparent backdrop:bg-transparent backdrop-blur-sm" ref="dialog">
-        <div class="fixed inset-0 flex flex-col justify-center items-center px-0 py-6 sm:px-4 z-50" scroll-region>
-            <transition enter-active-class="ease-out duration-300" enter-from-class="opacity-0"
-                enter-to-class="opacity-100" leave-active-class="ease-in duration-200" leave-from-class="opacity-100"
-                leave-to-class="opacity-0">
-                <div v-show="show" class="fixed inset-0 transform transition-all" @click="close">
-                    <div class="absolute inset-0 bg-gray-950 opacity-75" />
-                </div>
-            </transition>
+    <teleport to="body">
+        <dialog class="z-50 m-0 min-h-full min-w-full bg-transparent backdrop:bg-transparent backdrop-blur-sm" ref="dialog">
+            <div class="fixed inset-0 flex flex-col justify-center items-center px-0 py-6 sm:px-4 z-50" scroll-region>
+                <transition enter-active-class="ease-out duration-300" enter-from-class="opacity-0"
+                    enter-to-class="opacity-100" leave-active-class="ease-in duration-200" leave-from-class="opacity-100"
+                    leave-to-class="opacity-0">
+                    <div v-show="show" class="fixed inset-0 transform transition-all" @click="close">
+                        <div class="absolute inset-0 bg-gray-950 opacity-75" />
+                    </div>
+                </transition>
 
-            <transition enter-active-class="ease-out duration-300"
-                enter-from-class="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-                enter-to-class="opacity-100 translate-y-0 sm:scale-100" leave-active-class="ease-in duration-200"
-                leave-from-class="opacity-100 translate-y-0 sm:scale-100"
-                leave-to-class="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
+                <transition enter-active-class="ease-out duration-300"
+                    enter-from-class="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                    enter-to-class="opacity-100 translate-y-0 sm:scale-100" leave-active-class="ease-in duration-200"
+                    leave-from-class="opacity-100 translate-y-0 sm:scale-100"
+                    leave-to-class="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
 
-                <div v-if="show && isFullScreen"
-                    class="absolute top-2 right-2 sm:top-6 sm:right-6 lg:top-10 lg:right-10 z-50">
-                    <Tooltip :text="__('Close')" location="bottom">
-                        <button type="button" @click="close"
-                            class="transition hover:scale-105 active:scale-95 hover:rotate-180 shadow-lg text-white bg-white bg-opacity-50 opacity-50 p-4 lg:p-6 rounded-full text-xl ms-auto inline-flex justify-center items-center"
-                            data-modal-toggle="crud-modal">
-                            <svg class="w-4 h-4 md:w-6 md:h-6 lg:w-8 lg:h-8 " aria-hidden="true"
-                                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                    stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
-                            </svg>
-                            <span class="sr-only">{{ __('Close modal') }}</span>
-                        </button>
-                    </Tooltip>
-                </div>
-            </transition>
+                    <div v-if="show && isFullScreen"
+                        class="absolute top-2 right-2 sm:top-6 sm:right-6 lg:top-10 lg:right-10 z-50">
+                        <Tooltip :text="__('Close')" location="bottom">
+                            <button type="button" @click="close"
+                                class="transition hover:scale-105 active:scale-95 hover:rotate-180 shadow-lg text-white bg-white bg-opacity-50 opacity-50 p-4 lg:p-6 rounded-full text-xl ms-auto inline-flex justify-center items-center"
+                                data-modal-toggle="crud-modal">
+                                <svg class="w-4 h-4 md:w-6 md:h-6 lg:w-8 lg:h-8 " aria-hidden="true"
+                                    xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                        stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                                </svg>
+                                <span class="sr-only">{{ __('Close modal') }}</span>
+                            </button>
+                        </Tooltip>
+                    </div>
+                </transition>
 
 
-            <transition enter-active-class="ease-out duration-300"
-                enter-from-class="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-                enter-to-class="opacity-100 translate-y-0 sm:scale-100" leave-active-class="ease-in duration-200"
-                leave-from-class="opacity-100 translate-y-0 sm:scale-100"
-                leave-to-class="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
+                <transition enter-active-class="ease-out duration-300"
+                    enter-from-class="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                    enter-to-class="opacity-100 translate-y-0 sm:scale-100" leave-active-class="ease-in duration-200"
+                    leave-from-class="opacity-100 translate-y-0 sm:scale-100"
+                    leave-to-class="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
 
-                <div v-show="show" :class="maxWidthClass"
-                    class="overflow-x-visible overflow-y-auto lg:mb-6 bg-white dark:bg-gray-800 text-gray-700 dark:text-gray-200 rounded-lg shadow-xl  scrollbar-thin transform transition-all w-full sm:w-full sm:mx-4 ">
-                    <slot v-if="showSlot" />
-                </div>
-            </transition>
-        </div>
-    </dialog>
+                    <div v-show="show" :class="maxWidthClass"
+                        class="overflow-x-visible overflow-y-auto lg:mb-6 bg-white dark:bg-gray-800 text-gray-700 dark:text-gray-200 rounded-lg shadow-xl  scrollbar-thin transform transition-all w-full sm:w-full sm:mx-4 ">
+                        <slot v-if="showSlot" />
+                    </div>
+                </transition>
+            </div>
+        </dialog>
+    </teleport>
 </template>
