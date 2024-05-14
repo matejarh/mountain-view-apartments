33ee84145a16703b8ccb7c0a6cd56aaab7ac34b2
Matej Arh <matej.arh@gmail.com>
2024-05-14 13:44:46 +0200

    add remove facilities

--- resources/js/Pages/Admin/Properties/_partials/FacilityComponent.vue
+++ resources/js/Pages/Admin/Properties/_partials/FacilityComponent.vue
@@ -2,22 +2,35 @@
 import Tooltip from '@/Components/Tooltip.vue';
 import SpinnerIcon from '@/Icons/SpinnerIcon.vue';
 import CloseXIcon from '@/Icons/CloseXIcon.vue';
-import { useForm } from '@inertiajs/vue3';
+import { useForm, usePage } from '@inertiajs/vue3';
+import TrashBinIcon from '@/Icons/TrashBinIcon.vue';
+import ConfirmationModal from '@/Components/ConfirmationModal.vue';
+import { ref } from 'vue';
 
-defineProps({
+const props = defineProps({
     facility: Object,
     direction: String,
 })
 
-const dForm = useForm({})
+const page = usePage()
 
-const aForm = useForm({})
+const detachForm = useForm({})
 
-const detach = (facility, property) => {
-    dForm.put(route('admin.properties.detach.facility', { facility: facility, property: property }))
+const attachForm = useForm({})
+
+const destroyForm = useForm({})
+
+const showDestroyConfirm = ref(false)
+
+const detach = () => {
+    detachForm.put(route('admin.properties.detach.facility', { facility: props.facility, property: page.props.property }))
 }
-const attach = (facility, property) => {
-    aForm.put(route('admin.properties.attach.facility', { facility: facility, property: property }))
+const attach = () => {
+    attachForm.put(route('admin.properties.attach.facility', { facility: props.facility, property: page.props.property }))
+}
+
+const destroy = () => {
+    destroyForm.delete(route('admin.facilities.destroy', props.facility))
 }
 
 </script>
@@ -28,31 +41,54 @@ const attach = (facility, property) => {
             class="inline-flex items-center px-2 py-1 me-2 text-sm font-medium text-amazon-800 bg-amazon-100 rounded dark:bg-amazon-900 dark:text-amazon-300">
             {{ __(facility.name) }}
             <Tooltip :text="__('Remove')">
-                <button type="button" @click="detach(facility, $page.props.property)"
-                    :class="{ 'opacity-25': dForm.processing || dForm.recentlySuccessful }"
-                    :disabled="dForm.processing || dForm.recentlySuccessful"
+                <button type="button" @click="detach"
+                    :class="{ 'opacity-25': detachForm.processing || detachForm.recentlySuccessful }"
+                    :disabled="detachForm.processing || detachForm.recentlySuccessful"
                     class="inline-flex items-center p-1  ms-2 text-sm text-amazon-400 bg-transparent rounded-sm hover:bg-amazon-200 hover:text-amazon-900 dark:hover:bg-amazon-800 dark:hover:text-amazon-300"
                     aria-label="Remove">
-                    <CloseXIcon class="w-2 h-2 " v-show="!dForm.processing" />
-                    <SpinnerIcon v-show="dForm.processing" class="animate-spin  h-2 w-2 " />
-                    <span class="sr-only">{{ __('Remove badge') }}</span>
+                    <CloseXIcon class="w-2 h-2 " v-show="!detachForm.processing" />
+                    <SpinnerIcon v-show="detachForm.processing" class="animate-spin  h-2 w-2 " />
+                    <span class="sr-only">{{ __('Remove') }}</span>
                 </button>
             </Tooltip>
         </span>
         <span v-if="direction === 'attach'" id="badge-dismiss-red"
             class="inline-flex items-center px-2 py-1 me-2 text-sm font-medium text-bittersweet-800 bg-bittersweet-100 rounded dark:bg-bittersweet-900 dark:text-bittersweet-300">
             {{ __(facility.name) }}
-            <Tooltip :text="__('Add')">
-                <button type="button" @click="attach(facility, $page.props.property)"
-                    :class="{ 'opacity-25': aForm.processing || aForm.recentlySuccessful }"
-                    :disabled="aForm.processing || aForm.recentlySuccessful"
+            <Tooltip :text="__('Attach')">
+                <button type="button" @click="attach"
+                    :class="{ 'opacity-25': attachForm.processing || attachForm.recentlySuccessful }"
+                    :disabled="attachForm.processing || attachForm.recentlySuccessful"
                     class="inline-flex items-center p-1  ms-2 text-sm text-bittersweet-400 bg-transparent rounded-sm hover:bg-bittersweet-200 hover:text-bittersweet-900 dark:hover:bg-bittersweet-800 dark:hover:text-bittersweet-300"
-                    aria-label="Remove">
-                    <CloseXIcon class="w-2 h-2 rotate-45" v-show="!aForm.processing" />
-                    <SpinnerIcon v-show="aForm.processing" class="animate-spin  h-2 w-2 " />
-                    <span class="sr-only">{{ __('Remove badge') }}</span>
+                    aria-label="Attach">
+                    <CloseXIcon class="w-2 h-2 rotate-45" v-show="!attachForm.processing" />
+                    <SpinnerIcon v-show="attachForm.processing" class="animate-spin  h-2 w-2 " />
+                    <span class="sr-only">{{ __('Add') }}</span>
+                </button>
+            </Tooltip>
+            <Tooltip :text="__('Delete')">
+                <button type="button" @click="showDestroyConfirm = true"
+                    :class="{ 'opacity-25': destroyForm.processing || destroyForm.recentlySuccessful }"
+                    :disabled="destroyForm.processing || destroyForm.recentlySuccessful"
+                    class="inline-flex items-center p-0  ms-2 text-sm text-bittersweet-400 bg-transparent rounded-sm hover:bg-bittersweet-200 hover:text-bittersweet-900 dark:hover:bg-bittersweet-800 dark:hover:text-bittersweet-300"
+                    aria-label="Destroy">
+
+                    <TrashBinIcon class="w-2.5 h-2.5 " v-show="!destroyForm.processing" />
+                    <SpinnerIcon v-show="destroyForm.processing" class="animate-spin  h-2 w-2 " />
+                    <span class="sr-only">{{ __('Destroy') }}</span>
                 </button>
             </Tooltip>
         </span>
     </div>
+    <ConfirmationModal :show="showDestroyConfirm" @close="showDestroyConfirm = false" @confirmed="destroy"
+        :form="destroyForm" :busy-text="__('Deleting Facility')" :recently-successful-text="__('Facility Deleted')">
+        <template #icon>
+            <TrashBinIcon class=" text-gray-400 dark:text-gray-500 w-11 h-11 mb-3.5 mx-auto" />
+        </template>
+        <template #content>
+            <p class="mb-4 text-gray-500 dark:text-gray-300 text-lg">{{ __('This will remove facility from the server.')}}</p>
+            <p class="mb-4 text-gray-500 dark:text-gray-300 text-sm">{{ __('This facility will also be removed from all other properties!')}}</p>
+            <p class="mb-4 text-gray-500 dark:text-gray-300 text-sm">{{ __('Are you sure you want to do this?')}}</p>
+        </template>
+    </ConfirmationModal>
 </template>
--- resources/js/Pages/Admin/Properties/_partials/ManageFacilitiesDialog.vue
+++ resources/js/Pages/Admin/Properties/_partials/ManageFacilitiesDialog.vue
@@ -7,6 +7,9 @@ import SelectInput from '@/Components/SelectInput.vue';
 import TextInput from '@/Components/TextInput.vue';
 import { reactive, ref, watchEffect } from 'vue';
 import FacilityComponent from './FacilityComponent.vue';
+import { useForm } from '@inertiajs/vue3';
+import CirclePlusIcon from '@/Icons/CirclePlusIcon.vue';
+import SpinnerIcon from '@/Icons/SpinnerIcon.vue';
 
 const emit = defineEmits(['close', 'updated'])
 
@@ -15,25 +18,34 @@ const props = defineProps({
     property: Object,
 })
 
-const detach = (facility) => {
-
+const form = useForm({
+    name: '',
+    text: ''
+})
+const store = () => {
+    form.clearErrors()
+    form.post(route('admin.facilities.store'), {
+        onSuccess: () => {
+            form.reset()
+        }
+    })
 }
 
 
 </script>
 
 <template>
-    <DialogModal max-width="screen" :show="show" @close="$emit('close')">
+    <DialogModal max-width="7xl" :show="show" @close="$emit('close')">
         <template #title>{{ __('Manage Facilities') }}</template>
 
         <template #content>
-            <div class="grid grid-cols-2 gap-6">
-                <div class="text-right relative ">
+            <div class="grid sm:grid-cols-2 gap-6">
+                <div class="order-2 sm:order-1 sm:text-right relative " >
                     <h5 class="font-bold">
                         {{ __('Attached') }}
 
                     </h5>
-                    <TransitionGroup name="list-detach" tag="ul" class="flex flex-wrap justify-end">
+                    <TransitionGroup name="list-detach" tag="ul" class="flex flex-wrap sm:justify-end">
 
                         <li class="right-0 block mb-2" v-for="facility, key in property.facilities" :key="facility.id">
 
@@ -41,18 +53,42 @@ const detach = (facility) => {
                         </li>
                     </TransitionGroup>
                 </div>
-                <div class="relative">
+                <div class="relative order-1 sm:order-2">
                     <h5 class="font-bold">
                         {{ __('Not attached') }}
 
                     </h5>
                     <TransitionGroup name="list-attach" tag="ul" class="flex flex-wrap ">
-                        <li class="left-0 block mb-2" v-for="facility, key in $page.props.facilities_not_in_property" :key="facility.id">
+                        <li class="left-0 block mb-2" v-for="facility, key in $page.props.facilities_not_in_property"
+                            :key="facility.id">
                             <FacilityComponent :key="facility.id" :facility="facility" direction="attach" />
                         </li>
                     </TransitionGroup>
+
                     <div class="" v-if="$page.props.facilities_not_in_property.length <= 0">
                         {{ __('All facilities are attached to property') }}
+                    </div>
+
+                    <hr class="my-4 md:my-4 border-gray-200 dark:border-gray-800 col-span-full" />
+
+                    <div class="">
+                        <InputLabel for="name" :value="`${__('Create New Facility')}`" />
+                        <div class="flex">
+                            <TextInput id="name" v-model="form.name" type="text" class="mt-1 block w-full" required
+                                autocomplete="name" :has-error="!!form.errors.name"
+                                :placeholder="__('Enter new facility name') + '...'" />
+
+                                <PrimaryButton type="button"
+                                :class="{ 'opacity-25': form.processing || form.recentlySuccessful || !form.isDirty }"
+                                :disabled="form.processing || form.recentlySuccessful || !form.isDirty" @click="store">
+                                <div class="flex items-center">
+                                    <SpinnerIcon v-if="form.processing"
+                                    class="animate-spin h-5 w-5 text-white dark:text-white" />
+                                    <CirclePlusIcon v-else class="h-5 w-5 text-white dark:text-white"  />
+                                </div>
+                            </PrimaryButton>
+                        </div>
+                        <InputError :message="form.errors.name" class="mt-2" />
                     </div>
                 </div>
             </div>
