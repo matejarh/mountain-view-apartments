c05e8977da368265a32befc82c4018717c1121e8
Matej Arh <matej.arh@gmail.com>
2024-05-19 13:25:44 +0200

    search by type

--- app/Filters/ActivityFilters.php
+++ app/Filters/ActivityFilters.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Filters;
+
 use Illuminate\Database\Eloquent\Builder;
 
 class ActivityFilters extends Filters
@@ -13,19 +14,21 @@ class ActivityFilters extends Filters
      * @param string $query
      * @return \Illuminate\Database\Eloquent\Builder
      */
-    public function search($query) :Builder
+    public function search($query): Builder
     {
 
         return $this->builder->when($query ?? null, function ($query, $search) {
 
-            $query->whereHas('owner', function ($q) use ($search) {
-                $q->where('first_name', 'like', '%' . $search . '%')
-                    ->orWhere('name', 'like', '%' . $search . '%')
-                    ->orWhere('last_name', 'like', '%' . $search . '%')
-                    ->orWhere('phone', 'like', '%' . $search . '%')
-                    ->orWhere('country', 'like', '%' . $search . '%')
-                    ->orWhere('email', 'like', '%' . $search . '%');
-            });
+            $query->where('type', 'like', '%' . $search . '%')
+                ->orWhere('subject_type', 'like', '%' . $search . '%')
+                ->orWhereHas('owner', function ($q) use ($search) {
+                    $q->where('first_name', 'like', '%' . $search . '%')
+                        ->orWhere('name', 'like', '%' . $search . '%')
+                        ->orWhere('last_name', 'like', '%' . $search . '%')
+                        ->orWhere('phone', 'like', '%' . $search . '%')
+                        ->orWhere('country', 'like', '%' . $search . '%')
+                        ->orWhere('email', 'like', '%' . $search . '%');
+                });
 
         });
 
