87446d293ec8c599eb42af484d86555afa8656c4
Matej Arh <matej.arh@gmail.com>
2024-05-20 20:07:45 +0200

    localizations

--- resources/js/Layouts/_partials/_default/PageFooter.vue
+++ resources/js/Layouts/_partials/_default/PageFooter.vue
@@ -11,7 +11,7 @@ const helpers = useHelperStore()
 
 
     <footer class="relative z-10 bg-gray-100  dark:bg-gray-950 ">
-        <ShapeBottom class="text-gray-100 dark:text-gray-950 absolute inset-y-full top-0 z-0 left-0 right-0 w-full rotate-180 drop-shadow-up-lg " />
+        <ShapeTop class="text-gray-100 dark:text-gray-950 absolute inset-y-full top-0 z-0 left-0 right-0 w-full rotate-180 drop-shadow-up-lg " />
         <div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
             <div class="md:flex md:justify-between">
                 <div class="mb-6 md:mb-0">
@@ -22,35 +22,35 @@ const helpers = useHelperStore()
                 </div>
                 <div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
                     <div>
-                        <h2 class="mb-6 text-sm font-semibold  uppercase text-gray-700  dark:text-white drop-shadow">Accomodations</h2>
+                        <h2 class="mb-6 text-sm font-semibold  uppercase text-gray-700  dark:text-white drop-shadow">{{__('Accomodations')}}</h2>
                         <ul class="text-gray-500 dark:text-gray-400 font-medium">
                             <li class="mb-4">
-                                <inertia-link href="/discover/bled" class="hover:underline ">Bled</inertia-link>
+                                <inertia-link :href="`${$page.props.locale}/discover/bled`" class="hover:underline ">{{__('Explore Bled')}}</inertia-link>
                             </li>
                             <li>
-                                <inertia-link href="/explore/nassfeld" class="hover:underline  ">Nassfeld</inertia-link>
+                                <inertia-link :href="`${$page.props.locale}/explore/nassfeld`" class="hover:underline  ">{{__('Discover Nassfeld')}}</inertia-link>
                             </li>
                         </ul>
                     </div>
                     <div>
-                        <h2 class="mb-6 text-sm font-semibold text-gray-700 uppercase dark:text-white">Follow us</h2>
+                        <h2 class="mb-6 text-sm font-semibold text-gray-700 uppercase dark:text-white">{{__('Follow us')}}</h2>
                         <ul class="text-gray-500 dark:text-gray-400 font-medium">
                             <li class="mb-4">
-                                <a href="https://facebook.com" class="hover:underline ">Facebook</a>
+                                <a href="https://facebook.com" class="hover:underline ">{{__('Facebook')}}</a>
                             </li>
                             <li>
-                                <a href="https://twitter.com" class="hover:underline">Twitter</a>
+                                <a href="https://twitter.com" class="hover:underline">{{__('Twitter')}}</a>
                             </li>
                         </ul>
                     </div>
                     <div>
-                        <h2 class="mb-6 text-sm font-semibold text-gray-700 uppercase dark:text-white">Services</h2>
+                        <h2 class="mb-6 text-sm font-semibold text-gray-700 uppercase dark:text-white">{{__('Services')}}</h2>
                         <ul class="text-gray-500 dark:text-gray-400 font-medium">
                             <li class="mb-4">
-                                <button @click="helpers.showContactDrawer" class="hover:underline">Reservation</button >
+                                <button @click="helpers.showContactDrawer" class="hover:underline">{{__('Reservation')}}</button >
                             </li>
                             <li>
-                                <inertia-link :href="`/${$page.props.locale}/contact-us`" class="hover:underline">Contact Us</inertia-link >
+                                <inertia-link :href="`/${$page.props.locale}/contact-us`" class="hover:underline">{{__('Contact Us')}}</inertia-link >
                             </li>
                         </ul>
                     </div>
@@ -59,7 +59,7 @@ const helpers = useHelperStore()
             <hr class="my-6 border-gray-700 sm:mx-auto dark:border-gray-200 lg:my-8" />
             <div class="sm:flex sm:items-center sm:justify-between">
                 <span class="text-sm text-gray-700 dark:text-gray-200 sm:text-center">© 2024 <inertia-link
-                        href="/" class="hover:underline">Mountain View Apartments™</inertia-link>. All Rights Reserved.
+                        href="/" class="hover:underline">Mountain View Apartments™</inertia-link>. {{__('All Rights Reserved')}}.
                 </span>
                 <div class="flex mt-4 sm:justify-center sm:mt-0">
                     <a href="#" class="text-gray-700 dark:text-gray-300 hover:text-gray-900 dark:hover:text-gray-100">
@@ -69,7 +69,7 @@ const helpers = useHelperStore()
                                 d="M6.135 3H8V0H6.135a4.147 4.147 0 0 0-4.142 4.142V6H0v3h2v9.938h3V9h2.021l.592-3H5V3.591A.6.6 0 0 1 5.592 3h.543Z"
                                 clip-rule="evenodd" />
                         </svg>
-                        <span class="sr-only">Facebook page</span>
+                        <span class="sr-only">{{__('Facebook page')}}</span>
                     </a>
 
                     <a href="#" class="text-gray-700 dark:text-gray-300 hover:text-gray-900 dark:hover:text-gray-100 ms-5">
@@ -79,7 +79,7 @@ const helpers = useHelperStore()
                                 d="M20 1.892a8.178 8.178 0 0 1-2.355.635 4.074 4.074 0 0 0 1.8-2.235 8.344 8.344 0 0 1-2.605.98A4.13 4.13 0 0 0 13.85 0a4.068 4.068 0 0 0-4.1 4.038 4 4 0 0 0 .105.919A11.705 11.705 0 0 1 1.4.734a4.006 4.006 0 0 0 1.268 5.392 4.165 4.165 0 0 1-1.859-.5v.05A4.057 4.057 0 0 0 4.1 9.635a4.19 4.19 0 0 1-1.856.07 4.108 4.108 0 0 0 3.831 2.807A8.36 8.36 0 0 1 0 14.184 11.732 11.732 0 0 0 6.291 16 11.502 11.502 0 0 0 17.964 4.5c0-.177 0-.35-.012-.523A8.143 8.143 0 0 0 20 1.892Z"
                                 clip-rule="evenodd" />
                         </svg>
-                        <span class="sr-only">Twitter page</span>
+                        <span class="sr-only">{{__('Twitter page')}}</span>
                     </a>
 
 
