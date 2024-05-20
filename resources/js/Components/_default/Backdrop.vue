5052794627aa65afc0a50b91a694da27997961e8
Matej Arh <matej.arh@gmail.com>
2024-05-20 22:03:07 +0200

    fix

--- resources/js/Components/_default/Backdrop.vue
+++ resources/js/Components/_default/Backdrop.vue
@@ -12,7 +12,7 @@ defineEmits(['close'])
 <template>
     <transition enter-active-class="ease-out duration-300" enter-from-class="opacity-0" enter-to-class="opacity-100"
         leave-active-class="ease-in duration-200" leave-from-class="opacity-100" leave-to-class="opacity-0">
-        <div v-show="show" class="fixed backdrop-blur inset-0 transform transition-all" @click="$emit('close')">
+        <div v-show="show" class="fixed z-40 backdrop-blur inset-0 transform transition-all" @click="$emit('close')">
             <div class="absolute inset-0 bg-gray-950 opacity-75" />
         </div>
     </transition>
