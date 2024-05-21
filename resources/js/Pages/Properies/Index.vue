b5a8ff94d8223a3139a08ca1d083010fccd58537
Matej Arh <matej.arh@gmail.com>
2024-05-21 15:45:14 +0200

    faq disabled for now

--- resources/js/Pages/Properies/Index.vue
+++ resources/js/Pages/Properies/Index.vue
@@ -43,7 +43,7 @@ const items = ref([
                     </PropertyCard>
                 </TransitionGroup>
 
-                <FaqSection />
+                <!-- <FaqSection /> -->
             </Wrapper>
         </div>
     </DefaultLayout>
