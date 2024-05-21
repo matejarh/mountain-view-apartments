6f20744773e734f634b8c23ed6f37bd5c771259b
Matej Arh <matej.arh@gmail.com>
2024-05-21 23:40:52 +0200

    refactored

--- resources/js/Components/_default/MobileNavigation/MobileNavigationItem.vue
+++ resources/js/Components/_default/MobileNavigation/MobileNavigationItem.vue
@@ -1,16 +1,16 @@
 <script setup>
 const props = defineProps({
     href: {
-        type:String,
-        default:null,
+        type: String,
+        default: null,
     },
     as: {
-        type:String,
-        default:'li',
+        type: String,
+        default: 'li',
     },
     active: {
         type: [String, Boolean],
-        default:false,
+        default: false,
     },
 
 })
@@ -21,29 +21,33 @@ defineEmits(['clicked'])
 <template>
     <component :is="as" v-if="href">
         <inertia-link @click="$emit('clicked')" :href="href"
-        class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
-            :class="{'bg-gray-100 dark:bg-gray-700' : $page.url === active}">
+            class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+            :class="{ 'bg-gray-100 dark:bg-gray-700': $page.url === active }">
 
             <div class="text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-            :class="{'text-gray-900 dark:text-white' : $page.url === active}">
+                :class="{ 'text-gray-900 dark:text-white': $page.url === active }">
                 <slot name="icon" />
             </div>
 
-            <span class="ms-3"><slot /></span>
+            <span class="ms-3">
+                <slot />
+            </span>
         </inertia-link>
     </component>
 
     <component :is="as" v-else>
         <button @click="$emit('clicked')"
-        class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
-            :class="{'bg-gray-100 hover:bg-gray-700' : $page.url === active}">
+            class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+            :class="{ 'bg-gray-100 hover:bg-gray-700': $page.url === active }">
 
             <div class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-            :class="{'text-gray-900 dark:text-white' : $page.url === active}">
+                :class="{ 'text-gray-900 dark:text-white': $page.url === active }">
                 <slot name="icon" />
             </div>
 
-            <span class="ms-3"><slot /></span>
+            <span class="ms-3">
+                <slot />
+            </span>
         </button>
     </component>
 </template>
--- resources/js/Layouts/_partials/_default/MobileNavigation.vue
+++ resources/js/Layouts/_partials/_default/MobileNavigation.vue
@@ -1,14 +1,14 @@
 <script setup>
-import MobileNavigationItem from '@/Components/_default/MobileNavigation/MobileNavigationItem.vue'
-import HomeIcon from '@/Icons/HomeIcon.vue'
-import BookOpenIcon from '@/Icons/BookOpenIcon.vue'
+import MobileNavigationItem from '@/Components/_default/MobileNavigation/MobileNavigationItem.vue';
+import HomeIcon from '@/Icons/HomeIcon.vue';
+import BookOpenIcon from '@/Icons/BookOpenIcon.vue';
 import LandmarkIcon from '@/Icons/LandmarkIcon.vue';
 import { useHelperStore } from '@/stores/helpers';
 import MobileThemeSwitch from '@/Components/_default/MobileThemeSwitch.vue';
 import HamburgerButton from '@/Components/_default/MobileNavigation/HamburgerButton.vue';
 import LanguageDropdown from '@/Components/_default/LanguageDropdown.vue';
 import CloseIcon from '@/Icons/CloseIcon.vue';
-import ReservationInquiry from '@/Components/_default/ReservationInquiry.vue';
+import ReservationInquiryForm from '@/Components/_default/ReservationInquiry.vue';
 import CalendarIcon from '@/Icons/CalendarIcon.vue';
 import AdjustmentsIcon from '@/Icons/AdjustmentsIcon.vue';
 import UserIcon from '@/Icons/UserIcon.vue';
@@ -18,30 +18,75 @@ import Backdrop from '@/Components/_default/Backdrop.vue';
 import { useScrollStore } from '@/stores/scroll';
 import { computed } from 'vue';
 
-
-const helpers = useHelperStore()
-const scroll = useScrollStore()
+const helpers = useHelperStore();
+const scroll = useScrollStore();
 
 /**
- * Determens id icons backgrounds should be shown
+ * Determine if icons backgrounds should be shown
  */
 const showBackgrounds = computed(() => {
     if (scroll.windowHeight > 0 && scroll.scrollPosition > 10) {
-        return scroll.windowHeight <= scroll.scrollPosition
+        return scroll.windowHeight <= scroll.scrollPosition;
     }
-    return false
-})
+    return false;
+});
 
-const logoutForm = useForm({})
+const logoutForm = useForm({});
 
 const logout = () => {
-    helpers.hideMobileDrawer()
+    helpers.hideMobileDrawer();
     logoutForm.post(route('logout'));
 };
+
+// Define an array of navigation items
+const navItems = [
+    {
+        key: 'home',
+        route: 'home',
+        label: 'Home',
+        icon: HomeIcon,
+        path: (locale) => `/${locale}/home`,
+    },
+    {
+        key: 'accommodations',
+        route: 'properties.index',
+        label: 'Apartments',
+        icon: LandmarkIcon,
+        path: (locale) => `/${locale}/accommodations`,
+    },
+    {
+        key: 'exploreBled',
+        route: 'explore.bled',
+        label: 'Explore Bled',
+        icon: LandmarkIcon,
+        path: (locale) => `/${locale}/explore/bled`,
+    },
+    {
+        key: 'exploreNassfeld',
+        route: 'discover.nassfeld',
+        label: 'Discover Nassfeld',
+        icon: LandmarkIcon,
+        path: (locale) => `/${locale}/discover/nassfeld`,
+    },
+    {
+        key: 'aboutUs',
+        route: 'aboutus',
+        label: 'About Us',
+        icon: LandmarkIcon,
+        path: (locale) => `/${locale}/about-us`,
+    },
+    {
+        key: 'contact',
+        route: 'contact.show',
+        label: 'Contact',
+        icon: BookOpenIcon,
+        path: (locale) => `/${locale}/contact`,
+    },
+];
 </script>
 
 <template>
-    <div class=" xl:hidden fixed top-0 left-0 z-50 w-full p-4 bg-transparent flex justify-between">
+    <div class="xl:hidden fixed top-0 left-0 z-50 w-full p-4 bg-transparent flex justify-between">
         <LanguageDropdown class="opacity-50 hover:opacity-100 active:opacity-100" :align="'left'" :no-label="true" />
 
         <div class="space-x-4">
@@ -49,144 +94,64 @@ const logout = () => {
                 :class="{ 'bg-gray-400 dark:bg-gray-700 drop-shadow-lg': showBackgrounds }" />
             <HamburgerButton v-show="!helpers.mobileDrawer" :show-backgrounds="showBackgrounds"
                 @click="helpers.showMobileDrawer" />
-
         </div>
         <teleport to="body">
             <Backdrop v-show="helpers.mobileDrawer" @close="helpers.hideMobileDrawer" />
 
             <div id="drawer-mobile"
-                class="fixed top-0 right-0 z-50 flex flex-col h-screen  transition-transform bg-white w-screen sm:w-80 dark:bg-gray-800"
-                :class="helpers.mobileDrawer ? 'translate-x-0' : 'translate-x-full'" tabindex="-1"
-                aria-labelledby="drawer-right-label">
-
+                class="fixed top-0 right-0 z-50 flex flex-col h-screen transition-transform bg-white w-screen sm:w-80 dark:bg-gray-800"
+                :class="helpers.mobileDrawer ? 'translate-x-0' : 'translate-x-full'" tabindex="-1">
                 <div class="p-4 flex-grow-0 relative border-b shadow border-gray-200 dark:border-gray-700">
-                    <h5 id="drawer-navigation-label"
-                        class="text-base font-semibold text-gray-500 uppercase dark:text-gray-400">{{ __('Menu') }}
-                    </h5>
+                    <h5 class="text-base font-semibold text-gray-500 uppercase dark:text-gray-400">{{ __('Menu') }}</h5>
                     <button @click="helpers.hideMobileDrawer" type="button"
                         class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 absolute top-2.5 end-2.5 inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white">
                         <CloseIcon class="w-5 h-5" />
                         <span class="sr-only">{{ __('Close menu') }}</span>
                     </button>
                 </div>
-                <div class="grow  overflow-y-auto scrollbar-thin flex flex-col justify-between">
+                <div class="grow overflow-y-auto scrollbar-thin scrollbar-track-gray-800 dark:scrollbar-track-gray-800 dark:scrollbar-thumb-gray-700 scrollbar-corner-transparent flex flex-col justify-between">
                     <div class="px-4 relative">
                         <div class="">
-                            <div class="pb-4  flex flex-col justify-between ">
-                                <div v-if="$page.props?.auth.user"
-                                    class="pt-4 mt-4 space-y-2 font-medium ">
+                            <div class="pb-4 flex flex-col justify-between">
+                                <div v-if="$page.props?.auth.user" class="pt-4 mt-4 space-y-2 font-medium">
                                     <div class="flex items-center gap-4">
-                                        <img class="w-10 h-10 rounded-full"
-                                            :src="$page.props?.auth.user.profile_photo_url" alt="">
+                                        <img class="w-10 h-10 rounded-full" :src="$page.props?.auth.user.profile_photo_url" alt="">
                                         <div class="font-medium dark:text-white">
                                             <div>{{ $page.props?.auth.user.name }}</div>
-                                            <div class="text-sm text-gray-500 dark:text-gray-400">{{
-                                                $page.props?.auth.user.email }}</div>
+                                            <div class="text-sm text-gray-500 dark:text-gray-400">{{ $page.props?.auth.user.email }}</div>
                                         </div>
                                     </div>
-
-                                    <MobileNavigationItem as="div" v-if="$page.props?.auth.user.is_admin"
-                                        @clicked="helpers.hideMobileDrawer" :active="`/admin/dashboard`"
-                                        :href="route('admin.dashboard.show')">
-                                        <template #icon>
-                                            <AdjustmentsIcon />
-                                        </template>
+                                    <MobileNavigationItem as="div" v-if="$page.props?.auth.user.is_admin" @clicked="helpers.hideMobileDrawer" :active="`/admin/dashboard`" :href="route('admin.dashboard.show')">
+                                        <template #icon><AdjustmentsIcon /></template>
                                         {{ __("Administration") }}
                                     </MobileNavigationItem>
-                                    <MobileNavigationItem as="div" @clicked="helpers.hideMobileDrawer"
-                                        :active="`/user/profile`" :href="route('profile.show')">
-                                        <template #icon>
-                                            <UserIcon />
-                                        </template>
+                                    <MobileNavigationItem as="div" @clicked="helpers.hideMobileDrawer" :active="`/user/profile`" :href="route('profile.show')">
+                                        <template #icon><UserIcon /></template>
                                         {{ __("My Profile") }}
                                     </MobileNavigationItem>
                                     <MobileNavigationItem as="div" :href="null" @clicked="logout">
-                                        <template #icon>
-                                            <SignOutIcon />
-                                        </template>
+                                        <template #icon><SignOutIcon /></template>
                                         {{ __("Sign out") }}
                                     </MobileNavigationItem>
                                 </div>
-
-                                <ul
-                                    class="pt-4 mt-4 space-y-2 font-medium border-t border-gray-200 dark:border-gray-700">
-                                    <MobileNavigationItem @clicked="helpers.hideMobileDrawer"
-                                        :active="`/${$page.props?.locale}`" :href="route('home', $page.props?.locale)">
-                                        <template #icon>
-                                            <HomeIcon class="w-5 h-5" />
-                                        </template>
-                                        {{ __("Home") }}
+                                <ul class="pt-4 mt-4 space-y-2 font-medium border-t border-gray-200 dark:border-gray-700">
+                                    <MobileNavigationItem v-for="item in navItems" :key="item.key" @clicked="helpers.hideMobileDrawer" :active="`${item.path($page.props?.locale)}`" :href="route(item.route, $page.props?.locale)">
+                                        <template #icon><component :is="item.icon" class="w-5 h-5" /></template>
+                                        {{ __(item.label) }}
                                     </MobileNavigationItem>
-                                    <MobileNavigationItem @clicked="helpers.hideMobileDrawer"
-                                        :active="`/${$page.props?.locale}/accomodations`"
-                                        :href="route('properties.index', $page.props?.locale)">
-                                        <template #icon>
-                                            <LandmarkIcon class="w-5 h-5" />
-                                        </template>
-                                        {{ __("Apartments") }}
-                                    </MobileNavigationItem>
-                                    <MobileNavigationItem @clicked="helpers.hideMobileDrawer"
-                                        :active="`/${$page.props?.locale}/explore/bled`"
-                                        :href="route('explore.bled', $page.props?.locale)">
-                                        <template #icon>
-                                            <LandmarkIcon class="w-5 h-5" />
-                                        </template>
-                                        {{ __("Explore Bled") }}
-                                    </MobileNavigationItem>
-                                    <MobileNavigationItem @clicked="helpers.hideMobileDrawer"
-                                        :active="`/${$page.props?.locale}/discover/nassfeld`"
-                                        :href="route('discover.nassfeld', $page.props?.locale)">
-                                        <template #icon>
-                                            <LandmarkIcon class="w-5 h-5" />
-                                        </template>
-                                        {{ __("Explore Nassfeld") }}
-                                    </MobileNavigationItem>
-                                    <!--                                 <MobileNavigationItem @clicked="helpers.hideMobileDrawer"
-                                        :active="`/${$page.props?.locale}/offers`"
-                                        :href="route('offers', $page.props?.locale)">
-                                        <template #icon>
-                                            <LandmarkIcon class="w-5 h-5" />
-                                        </template>
-                                        {{ __("Offers") }}
-                                    </MobileNavigationItem> -->
-                                    <MobileNavigationItem @clicked="helpers.hideMobileDrawer"
-                                        :active="`/${$page.props?.locale}/about-us`"
-                                        :href="route('aboutus', $page.props?.locale)">
-                                        <template #icon>
-                                            <LandmarkIcon class="w-5 h-5" />
-                                        </template>
-                                        {{ __("About Us") }}
-                                    </MobileNavigationItem>
-                                    <MobileNavigationItem @clicked="helpers.hideMobileDrawer"
-                                        :active="`/${$page.props?.locale}/contact`"
-                                        :href="route('contact.show', $page.props?.locale)">
-                                        <template #icon>
-                                            <BookOpenIcon class="w-5 h-5" />
-                                        </template>
-                                        {{ __("Contact") }}
-                                    </MobileNavigationItem>
-
-
                                 </ul>
-
                             </div>
-
                         </div>
-
                     </div>
                     <div class="pt-4 mt-4 space-y-2 font-medium border-t border-gray-200 dark:border-gray-700 mx-4">
-                        <h5
-                            class="inline-flex items-center mb-6 text-base font-semibold text-gray-500 uppercase dark:text-gray-400">
+                        <h5 class="inline-flex items-center mb-6 text-base font-semibold text-gray-500 uppercase dark:text-gray-400">
                             <CalendarIcon class="w-3.5 h-3.5 me-2.5" />
                             {{ __('Reservation Inquiry') }}
                         </h5>
-                        <ReservationInquiry @close="helpers.hideMobileDrawer" />
+                        <ReservationInquiryForm @close="helpers.hideMobileDrawer" />
                     </div>
                 </div>
-
-
             </div>
         </teleport>
     </div>
-
 </template>
