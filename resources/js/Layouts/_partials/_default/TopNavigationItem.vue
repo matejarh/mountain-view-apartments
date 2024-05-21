461f514ee2626046bea7ec8cb639ec4e0a85246d
Matej Arh <matej.arh@gmail.com>
2024-05-21 23:06:20 +0200

    top items shoe active

--- resources/js/Layouts/_partials/_default/TopNavigationItem.vue
+++ resources/js/Layouts/_partials/_default/TopNavigationItem.vue
@@ -14,13 +14,13 @@ defineProps({
 <template>
     <li>
         <inertia-link v-if="as === 'link'" :href="to"
-            class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100  dark:text-white  dark:hover:bg-gray-700 dark:hover:text-white dark:border-gray-700"
-            :class="{ 'bg-primary-700 text-white hover:bg-primary-600 dark:bg-primary-800 dark:hover:bg-primary-700 shadow-lg': $page.url.startsWith(to) }" aria-current="page">
+            class="block py-2 px-3 text-gray-900 rounded-lg hover:bg-gray-100  dark:text-white  dark:hover:bg-gray-700 dark:hover:text-white dark:border-gray-700"
+            :class="{ 'bg-primary-700  text-white hover:bg-primary-600 dark:bg-primary-800 dark:hover:bg-primary-700 shadow-lg': $page.url.startsWith(to) }" aria-current="page">
             <slot />
         </inertia-link>
 
         <button v-else
-            class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100  dark:text-white  dark:hover:bg-gray-700 dark:hover:text-white dark:border-gray-700"
+            class="block py-2 px-3 rounded-lg text-gray-900 hover:bg-gray-100  dark:text-white  dark:hover:bg-gray-700 dark:hover:text-white dark:border-gray-700"
             >
             <slot />
         </button>
