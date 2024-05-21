2002d9472e10e4a8234cb1ac1081406d1ca3d581
Matej Arh <matej.arh@gmail.com>
2024-05-21 23:39:33 +0200

    cleanup

--- resources/js/Components/_default/ReservationInquiry.vue
+++ resources/js/Components/_default/ReservationInquiry.vue
@@ -1,7 +1,7 @@
 <script setup>
 import VueDatePicker from '@vuepic/vue-datepicker';
 import '@vuepic/vue-datepicker/dist/main.css'
-import { computed, onMounted, ref } from 'vue';
+import { computed, ref } from 'vue';
 import { useHelperStore } from '@/stores/helpers';
 import InputLabel from '../InputLabel.vue';
 import TextInput from '../TextInput.vue';
@@ -17,7 +17,7 @@ import { useReCaptcha } from 'vue-recaptcha-v3';
 
 const emit = defineEmits(['close'])
 
-const { executeRecaptcha, recaptchaLoaded, /* recaptchaInstance */ } = useReCaptcha()
+const { executeRecaptcha, recaptchaLoaded } = useReCaptcha()
 const page = usePage()
 
 const form = useForm({
@@ -36,8 +36,6 @@ const form = useForm({
 
 const datepickerRange = page.props?.settings?.find(setting => setting.slug === 'datepicker-range')
 
-
-// const date = ref(null)
 const disabledDates = ref([])
 const helpers = useHelperStore()
 const options = ref({
@@ -156,13 +154,6 @@ const store = () => {
             </div>
 
         </div>
-        <!-- <div class="mt-4">
-            <InputLabel :value="__('Message')" for="message" />
-
-            <TextArea id="message" v-model="form.message" type="text" class="mt-1 block w-full" required
-                autocomplete="message" :has-error="!!form.errors.message" :placeholder="__('Enter message') + '...'" />
-            <InputError :message="form.errors.description" class="mt-2" />
-        </div> -->
 
         <div class="mt-4">
             <InputLabel :value="__('Apartmant')" for="Apartmant" />
@@ -189,32 +180,6 @@ const store = () => {
                             ></VueDatePicker>
 
         </div>
-        <!-- <div class="mb-4">
-            <label for="guests" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Invite
-                guests</label>
-            <div class="relative">
-                <input type="search" id="guests"
-                    class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                    placeholder="Add guest email" required />
-                <button type="button"
-                    class="absolute inline-flex items-center px-3 py-1 text-sm font-medium text-white bg-primary-700 rounded-lg end-2 bottom-2 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"><svg
-                        class="w-3 h-3 me-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                        fill="currentColor" viewBox="0 0 20 18">
-                        <path
-                            d="M6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Zm11-3h-2V5a1 1 0 0 0-2 0v2h-2a1 1 0 1 0 0 2h2v2a1 1 0 0 0 2 0V9h2a1 1 0 1 0 0-2Z" />
-                    </svg>Add</button>
-            </div>
-        </div> -->
-        <!-- <div class="flex mb-4 -space-x-4 rtl:space-x-reverse">
-            <img class="w-8 h-8 border-2 border-white rounded-full dark:border-gray-800"
-                src="/docs/images/people/profile-picture-5.jpg" alt="">
-            <img class="w-8 h-8 border-2 border-white rounded-full dark:border-gray-800"
-                src="/docs/images/people/profile-picture-2.jpg" alt="">
-            <img class="w-8 h-8 border-2 border-white rounded-full dark:border-gray-800"
-                src="/docs/images/people/profile-picture-3.jpg" alt="">
-            <img class="w-8 h-8 border-2 border-white rounded-full dark:border-gray-800"
-                src="/docs/images/people/profile-picture-4.jpg" alt="">
-        </div> -->
         <div class="mt-4">
 
             <PrimaryButton type="submit"
@@ -229,17 +194,8 @@ const store = () => {
                 </div>
             </PrimaryButton>
         </div>
-        <!--
-        <button type="submit"
-            class="text-white mt-4 justify-center flex items-center bg-primary-700 hover:bg-primary-800 w-full focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800"><svg
-                class="w-3.5 h-3.5 me-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
-                viewBox="0 0 20 20">
-                <path
-                    d="M18 2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2ZM2 18V7h6.7l.4-.409A4.309 4.309 0 0 1 15.753 7H18v11H2Z" />
-                <path
-                    d="M8.139 10.411 5.289 13.3A1 1 0 0 0 5 14v2a1 1 0 0 0 1 1h2a1 1 0 0 0 .7-.288l2.886-2.851-3.447-3.45ZM14 8a2.463 2.463 0 0 0-3.484 0l-.971.983 3.468 3.468.987-.971A2.463 2.463 0 0 0 14 8Z" />
-            </svg> {{__('Submit')}}</button> -->
     </form>
+
     <ConfirmationModal :is-danger="false" :show="showInquiryConfirmationModal" @close="showInquiryConfirmationModal = false" @confirmed="recaptcha"
             :form="form" busy-text="Sending inquiry" recently-successful-text="Inquiry sent" button-text="Send inquiry">
             <template #icon>
