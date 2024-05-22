3d110d4cd14fc90cb99da83ba74ce115af4fa823
Matej Arh <matej.arh@gmail.com>
2024-05-22 10:01:25 +0200

    breakpoints

--- resources/js/Components/_default/ActiveCarousel.vue
+++ resources/js/Components/_default/ActiveCarousel.vue
@@ -1,5 +1,5 @@
 <template>
-    <Carousel :itemsToShow="1.95" :autoplay="5000" :wrapAround="true" :transition="500">
+    <Carousel :itemsToShow="1.95" :autoplay="5000" :wrapAround="true" :transition="500" :breakpoints="breakpoints">
         <Slide v-for="slide, key in items" :key="key">
             <div class="carousel__item" @click="">
                 <img :src="slide.thumb_url" class="w-auto h-full">
@@ -14,19 +14,45 @@
 </template>
 
 <script setup>
+import { ref } from 'vue';
 import { Carousel, Navigation, Pagination, Slide } from 'vue3-carousel'
 
 import 'vue3-carousel/dist/carousel.css'
 
 const props = defineProps({
-    items: Array
+    items: Array,
+    toShow: {
+        default: 1.95,
+        type: Number,
+    },
 })
+
+const breakpoints = ref({
+      640: {
+        itemsToShow: 2.95,
+        snapAlign: 'center',
+      },
+      768: {
+        itemsToShow: 2.95,
+        snapAlign: 'center',
+      },
+      1024: {
+        itemsToShow: 2.25,
+        snapAlign: 'center',
+      },
+      1280: {
+        itemsToShow: 2.75,
+        snapAlign: 'center',
+      },
+    },
+)
 
 </script>
 
 <style scoped>
 .carousel__slide {
     padding: 5px;
+
 }
 
 .carousel__viewport {
@@ -43,25 +69,26 @@ const props = defineProps({
 
 .carousel__slide {
     opacity: 0.9;
-    transform: rotateY(-20deg) scale(0.9);
+    transform: rotateY(-33deg) scale(0.9);
 }
 
 .carousel__slide--active~.carousel__slide {
-    transform: rotateY(20deg) scale(0.9);
+    transform: rotateY(33deg) scale(0.9);
 }
 
 .carousel__slide--prev {
     opacity: 1;
-    transform: rotateY(-10deg) scale(0.95);
+    transform: rotateY(-33deg) scale(0.9);
 }
 
 .carousel__slide--next {
     opacity: 1;
-    transform: rotateY(10deg) scale(0.95);
+    transform: rotateY(33deg) scale(0.9);
 }
 
 .carousel__slide--active {
     opacity: 1;
-    transform: rotateY(0) scale(1.1);
+    transform: rotateY(0) scale(1.2);
+    border-radius: 0.5rem;
 }
 </style>
