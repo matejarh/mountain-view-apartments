0186b0e23f7568c471525e8309f4ca7da005a802
Matej Arh <matej.arh@gmail.com>
2024-05-22 17:44:40 +0200

    no reviews

--- resources/js/Pages/Reviews/Index.vue
+++ resources/js/Pages/Reviews/Index.vue
@@ -14,10 +14,13 @@ import Wrapper from '@/Components/_default/Wrapper.vue';
             <HeroSection />
             <ShapedSection :color="{ dark: 'primary-900', light: 'primary-700' }">
                 <div class="py-8 lg:py-16 mx-auto max-w-screen-md px-4">
-                    <h2
+                    <h2 v-if="$page.props?.reviews.data.length > 0"
                         class="mb-4 text-2xl font-extrabold tracking-tight leading-none text-white md:text-3xl lg:text-4xl">
-                        {{ __('Reviews') }}
+                        {{ __('Reviews')  }}
                     </h2>
+                    <p class="mb-4 text-2xl text-center font-extrabold tracking-tight leading-none text-white md:text-3xl lg:text-2xl" v-else>
+                        {{ __('No reviews yet') }}
+                    </p>
                     <ReviewCard v-for="review in $page.props?.reviews.data" :key="review.id" :review="review">
                     </ReviewCard>
                 </div>
