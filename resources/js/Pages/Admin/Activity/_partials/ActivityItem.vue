aa055a0d2f54f0cdeb6973fc55f40090a5137a1d
Matej Arh <matej.arh@gmail.com>
2024-05-22 12:06:23 +0200

    fix on subject

--- resources/js/Pages/Admin/Activity/_partials/ActivityItem.vue
+++ resources/js/Pages/Admin/Activity/_partials/ActivityItem.vue
@@ -13,34 +13,34 @@ defineProps({
     <li class="mb-10 ms-6">
         <span
             class="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -start-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
-            <img class="rounded-full shadow-lg" :src="item.owner.profile_photo_url"
-                :alt="`${item.owner.name}s avatar`" />
+            <img class="rounded-full shadow-lg" :src="item?.owner.profile_photo_url"
+                :alt="`${item?.owner.name}s avatar`" />
         </span>
         <div
             class="p-4 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-700 dark:border-gray-600">
             <div class="items-center justify-between mb-3 sm:flex">
                 <time class="mb-1 text-xs font-normal text-gray-400 sm:order-last sm:mb-0">{{
-                    item.created_at_diff_for_humans }} from {{ item.location.cityName }}, {{ item.location.countryName }}</time>
+                    item?.created_at_diff_for_humans }} from {{ item?.location.cityName }}, {{ item?.location.countryName }}</time>
 
                 <div class="text-sm font-normal text-gray-500 lex dark:text-gray-300">
-                    {{ item.owner.name }}
-                    {{ __(item.type) }}
+                    {{ item?.owner.name }}
+                    {{ __(item?.type) }}
                     <a href="#" class="font-semibold text-gray-900 dark:text-white hover:underline">
-                        {{ item.subject?.name }}
-                        <span v-if="item.type === 'created_like'">{{ item.subject?.liked_trimed[0].title[$page.props?.locale] }}</span>
-                        <span v-if="item.type === 'created_review'">{{ item.subject.reviewed_trimed[0].title[$page.props?.locale] }}</span></a>
+                        {{ item?.subject?.name }}
+                        <span v-if="item?.type === 'created_like'">{{ item?.subject?.liked_trimed[0].title[$page.props?.locale] }}</span>
+                        <span v-if="item?.type === 'created_review'">{{ item?.subject?.reviewed_trimed[0].title[$page.props?.locale] }}</span></a>
                 </div>
             </div>
             <div
                 class="p-3 text-xs italic font-normal text-gray-500 border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-600 dark:border-gray-500 dark:text-gray-300">
                 <div class="flex items-center  whitespace-nowrap ">
-                    <Tooltip text="Desktop" v-if="item.agent.is_desktop">
+                    <Tooltip text="Desktop" v-if="item?.agent.is_desktop">
                         <DesktopIcon class="w-8 h-8 z-0" />
                     </Tooltip>
-                    <Tooltip text="Mobile" v-else-if="item.agent.is_mobile">
+                    <Tooltip text="Mobile" v-else-if="item?.agent.is_mobile">
                         <MobileIcon class="w-8 h-8 z-0" />
                     </Tooltip>
-                    <Tooltip text="Tablet" v-else-if="item.agent.is_tablet">
+                    <Tooltip text="Tablet" v-else-if="item?.agent.is_tablet">
                         <TabletIcon class="w-8 h-8 z-0" />
                     </Tooltip>
                     <Tooltip text="Unknown" v-else>
@@ -49,10 +49,10 @@ defineProps({
                     <div class="ps-3 leading-tight">
                         <p class="text-base">
 
-                            {{ item.agent.browser }}
+                            {{ item?.agent.browser }}
                         </p>
                         <p class="font-normal text-gray-500 dark:text-gray-300">
-                            {{ item.agent.platform }}
+                            {{ item?.agent.platform }}
                         </p>
 
                     </div>
