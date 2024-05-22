90f9face3b98fb2dbe39b51d62894ce0c9263406
Matej Arh <matej.arh@gmail.com>
2024-05-22 08:53:11 +0200

    localization

--- resources/js/Pages/Welcome/_partials/DiscoverSection.vue
+++ resources/js/Pages/Welcome/_partials/DiscoverSection.vue
@@ -25,7 +25,7 @@ import ShapedSection from '@/Components/_default/ShapedSection.vue';
                         </p>
                         <inertia-link :href="item.link"
                             class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-primary-700 rounded-lg hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
-                            Read more
+                            {{ __('Read more') }}
                             <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true"
                                 xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
