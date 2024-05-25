4f17d7595bf24b8b4acab77ecba13a24836aa626
Matej Arh <matej.arh@gmail.com>
2024-05-26 00:27:11 +0200

    fresh

--- database/seeders/ExportPagesGalleriesTableSeeder.php
+++ database/seeders/ExportPagesGalleriesTableSeeder.php
@@ -14,21 +14,37 @@ class ExportPagesGalleriesTableSeeder extends Seeder
      */
     public function run()
     {
-        
+
 
         \DB::table('pages_galleries')->delete();
-        
+
         \DB::table('pages_galleries')->insert(array (
-            0 => 
+            0 =>
             array (
                 'id' => 1,
+                'gallery_id' => 8,
+                'page_id' => 7,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+            ),
+            1 =>
+            array (
+                'id' => 2,
+                'gallery_id' => 9,
+                'page_id' => 8,
+                'created_at' => NULL,
+                'updated_at' => NULL,
+            ),
+            2 =>
+            array (
+                'id' => 3,
                 'gallery_id' => 4,
                 'page_id' => 1,
                 'created_at' => NULL,
                 'updated_at' => NULL,
             ),
         ));
-        
-        
+
+
     }
 }
