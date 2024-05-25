ccbc05322c20669c355889118665ea91d64dca9d
Matej Arh <matej.arh@gmail.com>
2024-05-25 13:49:50 +0200

    more buttons in a row

---
+++ resources/views/vendor/mail/html/buttons.blade.php
@@ -0,0 +1,32 @@
+@props([
+    'align' => 'center',
+    'buttons' => [
+        [
+            'url',
+            'color' => 'primary',
+            'align' => 'center',
+            'text' => 'Button Text'
+        ]
+    ]
+])
+<table class="action" align="{{ $align }}" width="100%" cellpadding="0" cellspacing="0" role="presentation">
+<tr>
+<td align="{{ $align }}">
+<table width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation">
+<tr>
+<td align="{{ $align }}">
+<table border="0" cellpadding="4" cellspacing="0" role="presentation">
+<tr>
+@foreach ($buttons as $button)
+<td>
+<a href="{{ $button['url'] }}" class="button button-{{ $button['color'] }}" target="_blank" rel="noopener">{{ $button['text'] }}</a>
+</td>
+@endforeach
+</tr>
+</table>
+</td>
+</tr>
+</table>
+</td>
+</tr>
+</table>
