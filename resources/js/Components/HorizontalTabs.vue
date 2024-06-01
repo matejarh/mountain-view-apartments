95e6cf46c5528f95220703a5033a549ef87978b2
Matej Arh <matej.arh@gmail.com>
2024-06-01 23:26:50 +0200

    sticky flags

--- resources/js/Components/HorizontalTabs.vue
+++ resources/js/Components/HorizontalTabs.vue
@@ -5,7 +5,7 @@
 <template>
 
 
-    <div class="border-b border-gray-200 dark:border-gray-700">
+    <div class="sticky top-0 border-b z-10 border-gray-200 bg-white dark:bg-gray-900 dark:border-gray-700">
         <ul class="flex flex-wrap -mb-px text-sm font-medium text-center text-gray-500 dark:text-gray-400">
             <slot name="header" />
         </ul>
