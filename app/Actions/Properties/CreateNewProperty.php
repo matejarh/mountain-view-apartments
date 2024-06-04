cddee069ee29c820ee5e086f6124f4d9a5aa5d99
Matej Arh <matej.arh@gmail.com>
2024-06-05 01:03:52 +0200

    refactor and fix missing lang keys

--- app/Actions/Properties/CreateNewProperty.php
+++ app/Actions/Properties/CreateNewProperty.php
@@ -7,38 +7,68 @@ use App\Rules\SpamFree;
 use Illuminate\Support\Facades\Validator;
 use App\Contracts\CreatesNewProperties;
 
-
 class CreateNewProperty implements CreatesNewProperties
 {
     /**
-     * Validate and create new property.
+     * Validate and create a new property.
      *
-     * @param  array<string, string>  $input
+     * @param array<string, string> $input The input data containing the property information.
+     * @return void
      */
-    public function create(array $input)
+    public function create(array $input): void
     {
-        $attributeNames = array(
+        $attributeNames = $this->getAttributeNames();
+        $rules = $this->getValidationRules();
+
+        $this->fillMissingLocales($input);
+
+        $validator = Validator::make($input, $rules);
+        $validator->setAttributeNames($attributeNames)->validateWithBag('creatingProperty');
+
+        Property::forceCreate($this->getPropertyAttributes($input));
+
+        cache()->forget('properties_list_for_dropdown');
+
+        session()->flash('flash.banner', __('Property has been created.'));
+    }
+
+    /**
+     * Get attribute names for validation.
+     *
+     * @return array Attribute names.
+     */
+    private function getAttributeNames(): array
+    {
+        return [
             'name' => __('Name'),
             'description' => __('Description'),
             'type' => __('Type'),
             'title' => __('Title'),
             'address' => __('Address'),
             'keywords' => __('Keywords'),
-            'is_entire_apartment' => __('Iis Entire Apartment'),
+            'is_entire_apartment' => __('Is Entire Apartment'),
             'bed_types' => __('Bed Types'),
-            'recomended' => __('Recomended'),
+            'recomended' => __('Recommended'),
             'rules' => __('Rules'),
-        );
+        ];
+    }
 
+    /**
+     * Get validation rules for creating a property.
+     *
+     * @return array Validation rules.
+     */
+    private function getValidationRules(): array
+    {
         $rules = [
-            'type' => ['required', 'string', 'max:255', new SpamFree] ,
+            'type' => ['required', 'string', 'max:255', new SpamFree],
             'name' => ['required', 'string', 'max:255', new SpamFree, 'unique:properties'],
             'description' => ['required', 'array', 'min:1'],
             'quote' => ['required', 'array', 'min:1'],
             'long_description' => ['required', 'array', 'min:1'],
             'keywords' => ['required', 'array', 'min:1'],
             'title' => ['required', 'array', 'min:1'],
-            'address' => ['required', 'string', 'max:255', new SpamFree] ,
+            'address' => ['required', 'string', 'max:255', new SpamFree],
             'is_entire_apartment' => ['required', 'boolean'],
             'coordinates' => ['required', 'array', 'min:2', 'max:2'],
             'bed_types' => ['required', 'array', 'min:1'],
@@ -49,63 +79,65 @@ class CreateNewProperty implements CreatesNewProperties
             'rules' => ['required', 'array', 'min:1'],
         ];
 
-        foreach (config('app.supported_locales') as $key => $value) {
-            if (!isset($input['title'][$value])) {
-                $input['title'][$value] = '';
-            }
-            if (!isset($input['quote'][$value])) {
-                $input['quote'][$value] = '';
-            }
-            if (!isset($input['description'][$value])) {
-                $input['description'][$value] = '';
-            }
-            if (!isset($input['long_description'][$value])) {
-                $input['long_description'][$value] = '';
-            }
-            if (!isset($input['keywords'][$value])) {
-                $input['keywords'][$value] = '';
-            }
-            if (!isset($input['rules'][$value])) {
-                $input['rules'][$value] = '';
-            }
-
+        foreach (config('app.supported_locales') as $locale) {
             $rules += [
-                "description.$value" => ['nullable', 'string', 'distinct', new SpamFree],
-                "quote.$value" => ['nullable', 'string', 'distinct', new SpamFree],
-                "long_description.$value" => ['nullable', 'string', 'distinct', new SpamFree],
-                "title.$value" => ['nullable', 'string', 'distinct', new SpamFree],
-                "keywords.$value" => ['nullable', 'string', 'distinct', new SpamFree],
-                "rules.$value" => ['required', 'array', 'distinct', 'min:1'],
-                "rules.$value.*.title" => ['nullable', 'string', new SpamFree],
-                "rules.$value.*.description" => ['nullable', 'string', new SpamFree],
-                "rules.$value.*.name" => ['nullable', 'string', new SpamFree],
+                "description.$locale" => ['nullable', 'string', 'distinct', new SpamFree],
+                "quote.$locale" => ['nullable', 'string', 'distinct', new SpamFree],
+                "long_description.$locale" => ['nullable', 'string', 'distinct', new SpamFree],
+                "title.$locale" => ['nullable', 'string', 'distinct', new SpamFree],
+                "keywords.$locale" => ['nullable', 'string', 'distinct', new SpamFree],
+                "rules.$locale" => ['required', 'array', 'distinct', 'min:0'],
+                "rules.$locale.*.title" => ['nullable', 'string', new SpamFree],
+                "rules.$locale.*.description" => ['nullable', 'string', new SpamFree],
+                "rules.$locale.*.name" => ['nullable', 'string', new SpamFree],
             ];
         }
 
-        $validator = Validator::make($input, $rules);
+        return $rules;
+    }
 
-        $validator->setAttributeNames($attributeNames)->validateWithBag('creatingProperty');
+    /**
+     * Fill missing locales in the input data.
+     *
+     * @param array $input The input data.
+     * @return void
+     */
+    private function fillMissingLocales(array &$input): void
+    {
+        foreach (config('app.supported_locales') as $locale) {
+            $input['title'][$locale] = $input['title'][$locale] ?? '';
+            $input['quote'][$locale] = $input['quote'][$locale] ?? '';
+            $input['description'][$locale] = $input['description'][$locale] ?? '';
+            $input['long_description'][$locale] = $input['long_description'][$locale] ?? '';
+            $input['keywords'][$locale] = $input['keywords'][$locale] ?? '';
+            $input['rules'][$locale] = $input['rules'][$locale] ?? $input['rules'][config('app.fallback_locale')] ?? [];
+        }
+    }
 
-        Property::forceCreate([
+    /**
+     * Get property attributes for creation.
+     *
+     * @param array $input The validated input data.
+     * @return array Property attributes.
+     */
+    private function getPropertyAttributes(array $input): array
+    {
+        return [
             'user_id' => auth()->id(),
             'name' => $input['name'],
             'type' => $input['type'],
             'slug' => str($input['name'])->slug(),
-            'description' => isset($input['description']) ? $input['description'] : null,
-            'quote' => isset($input['quote']) ? $input['quote'] : null,
-            'long_description' => isset($input['long_description']) ? $input['long_description'] : null,
-            'title' => isset($input['title']) ? $input['title'] : null,
-            'address' => isset($input['address']) ? $input['address'] : null,
-            'keywords' => isset($input['keywords']) ? $input['keywords'] : null,
+            'description' => $input['description'] ?? null,
+            'quote' => $input['quote'] ?? null,
+            'long_description' => $input['long_description'] ?? null,
+            'title' => $input['title'] ?? null,
+            'address' => $input['address'] ?? null,
+            'keywords' => $input['keywords'] ?? null,
             'is_entire_apartment' => $input['is_entire_apartment'],
-            'coordinates' => isset($input['coordinates']) ? $input['coordinates'] : null,
-            'bed_types' => isset($input['bed_types']) ? $input['bed_types'] : null,
-            'recomended' => isset($input['recomended']) ? $input['recomended'] : null,
-            'rules' => isset($input['rules']) ? $input['rules'] : null,
-        ]);
-
-        cache()->forget('properties_list_for_dropdown');
-
-        session()->flash('flash.banner', __('Property has been created.'));
+            'coordinates' => $input['coordinates'] ?? null,
+            'bed_types' => $input['bed_types'] ?? null,
+            'recomended' => $input['recomended'] ?? null,
+            'rules' => $input['rules'] ?? null,
+        ];
     }
 }
--- app/Actions/Properties/UpdateProperty.php
+++ app/Actions/Properties/UpdateProperty.php
@@ -8,42 +8,64 @@ use Illuminate\Support\Facades\Validator;
 use App\Contracts\UpdatesProperties;
 use Illuminate\Validation\Rule;
 
-
 class UpdateProperty implements UpdatesProperties
 {
     /**
      * Update the specified property.
      *
-     * @param  Property $property The property instance to be updated.
-     * @param  array $input The input data containing the updated property information.
+     * @param Property $property The property instance to be updated.
+     * @param array $input The input data containing the updated property information.
      * @return void
      */
     public function update(Property $property, array $input): void
     {
-        $attributeNames = array(
+        $attributeNames = [
             'name' => __('Name'),
             'description' => __('Description'),
             'type' => __('Type'),
             'address' => __('Address'),
             'keywords' => __('Keywords'),
-            'is_entire_apartment' => __('Iis Entire Apartment'),
+            'is_entire_apartment' => __('Is Entire Apartment'),
             'bed_types' => __('Bed Types'),
             'facilities' => __('Facilities'),
-            'recomended' => __('Recomended'),
+            'recomended' => __('Recommended'),
             'rules' => __('Rules'),
             'coordinates.lng' => __('Longitude'),
             'coordinates.lat' => __('Latitude'),
-        );
+        ];
 
+        $rules = $this->getValidationRules($property);
+
+        $this->fillMissingLocales($input);
+
+        $validator = Validator::make($input, $rules);
+
+        $validator->setAttributeNames($attributeNames)->validateWithBag('updatingProperty');
+
+        $this->updatePropertyAttributes($property, $input);
+
+        cache()->forget('properties_list_for_dropdown');
+
+        session()->flash('flash.banner', __('Property has been updated.'));
+    }
+
+    /**
+     * Get validation rules for updating a property.
+     *
+     * @param Property $property The property instance.
+     * @return array Validation rules.
+     */
+    private function getValidationRules(Property $property): array
+    {
         $rules = [
-            'type' => ['required', 'string', 'max:255', new SpamFree] ,
+            'type' => ['required', 'string', 'max:255', new SpamFree],
             'name' => ['required', 'string', 'max:255', new SpamFree, Rule::unique('properties')->ignore($property->id)],
             'description' => ['required', 'array', 'min:1'],
             'quote' => ['required', 'array', 'min:1'],
             'long_description' => ['required', 'array', 'min:1'],
             'keywords' => ['required', 'array', 'min:1'],
             'title' => ['required', 'array', 'min:1'],
-            'address' => ['required', 'string', 'max:255', new SpamFree] ,
+            'address' => ['required', 'string', 'max:255', new SpamFree],
             'is_entire_apartment' => ['required', 'boolean'],
             'coordinates' => ['required', 'array', 'min:2', 'max:2'],
             'bed_types' => ['required', 'array', 'min:1'],
@@ -54,44 +76,65 @@ class UpdateProperty implements UpdatesProperties
             'rules' => ['required', 'array', 'min:1'],
         ];
 
-        foreach (config('app.supported_locales') as $key => $value) {
+        foreach (config('app.supported_locales') as $locale) {
             $rules += [
-                "description.$value" => ['nullable', 'string', 'distinct', new SpamFree],
-                "quote.$value" => ['nullable', 'string', 'distinct', new SpamFree],
-                "long_description.$value" => ['nullable', 'string', 'distinct', new SpamFree],
-                "title.$value" => ['nullable', 'string', 'distinct', new SpamFree],
-                "keywords.$value" => ['nullable', 'string', 'distinct', new SpamFree],
-                "rules.$value" => ['required', 'array', 'distinct', 'min:1'],
-                "rules.$value.*.title" => ['nullable', 'string', new SpamFree],
-                "rules.$value.*.description" => ['nullable', 'string', new SpamFree],
-                "rules.$value.*.name" => ['nullable', 'string', new SpamFree],
+                "description.$locale" => ['nullable', 'string', 'distinct', new SpamFree],
+                "quote.$locale" => ['nullable', 'string', 'distinct', new SpamFree],
+                "long_description.$locale" => ['nullable', 'string', 'distinct', new SpamFree],
+                "title.$locale" => ['nullable', 'string', 'distinct', new SpamFree],
+                "keywords.$locale" => ['nullable', 'string', 'distinct', new SpamFree],
+                "rules.$locale" => ['required', 'array', 'distinct', 'min:0'],
+                "rules.$locale.*.title" => ['nullable', 'string', new SpamFree],
+                "rules.$locale.*.description" => ['nullable', 'string', new SpamFree],
+                "rules.$locale.*.name" => ['nullable', 'string', new SpamFree],
             ];
         }
 
-        // dd($input['long_description']);
-        $validator = Validator::make($input, $rules);
+        return $rules;
+    }
 
-        $validator->setAttributeNames($attributeNames)->validateWithBag('updatingProperty');
+    /**
+     * Fill missing locales in the input data.
+     *
+     * @param array $input The input data.
+     * @return void
+     */
+    private function fillMissingLocales(array &$input): void
+    {
+        foreach (config('app.supported_locales') as $locale) {
+            $input['title'][$locale] = $input['title'][$locale] ?? '';
+            $input['quote'][$locale] = $input['quote'][$locale] ?? '';
+            $input['description'][$locale] = $input['description'][$locale] ?? '';
+            $input['long_description'][$locale] = $input['long_description'][$locale] ?? '';
+            $input['keywords'][$locale] = $input['keywords'][$locale] ?? '';
+            $input['rules'][$locale] = $input['rules'][$locale] ?? $input['rules'][config('app.fallback_locale')] ?? [];
+        }
+    }
 
+    /**
+     * Update property attributes with validated input data.
+     *
+     * @param Property $property The property instance.
+     * @param array $input The validated input data.
+     * @return void
+     */
+    private function updatePropertyAttributes(Property $property, array $input): void
+    {
         $property->forceFill([
             'name' => $input['name'],
             'type' => $input['type'],
             'slug' => str($input['name'])->slug(),
-            'description' => isset($input['description']) ? $input['description'] : $property->description,
-            'quote' => isset($input['quote']) ? $input['quote'] : $property->quote,
-            'long_description' => isset($input['long_description']) ? $input['long_description'] : $property->long_description,
-            'title' => isset($input['title']) ? $input['title'] : $property->title,
-            'address' => isset($input['address']) ? $input['address'] : $property->address,
-            'keywords' => isset($input['keywords']) ? $input['keywords'] : $property->keywords,
+            'description' => $input['description'],
+            'quote' => $input['quote'],
+            'long_description' => $input['long_description'],
+            'title' => $input['title'],
+            'address' => $input['address'],
+            'keywords' => $input['keywords'],
             'is_entire_apartment' => $input['is_entire_apartment'],
-            'coordinates' => isset($input['coordinates']) ? $input['coordinates'] : $property->coordinates,
-            'bed_types' => isset($input['bed_types']) ? $input['bed_types'] : $property->bed_types,
-            'recomended' => isset($input['recomended']) ? $input['recomended'] : $property->recomended,
-            'rules' => isset($input['rules']) ? $input['rules'] : $property->rules,
+            'coordinates' => $input['coordinates'],
+            'bed_types' => $input['bed_types'],
+            'recomended' => $input['recomended'],
+            'rules' => $input['rules'],
         ])->save();
-
-        cache()->forget('properties_list_for_dropdown');
-
-        session()->flash('flash.banner', __('Property has been updated.'));
     }
 }
