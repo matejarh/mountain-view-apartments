d5117c7e5b8d935645b2c53648aea7f2a4d0f245
Matej Arh <matej.arh@gmail.com>
2024-06-13 10:37:56 +0200

    is active

--- app/Models/Price.php
+++ app/Models/Price.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -17,6 +19,10 @@ class Price extends Model
         'discounts' => 'array',
     ];
 
+    protected $appends = [
+        'is_active',
+    ];
+
     /**
      * Get the property who owns this price.
      *
@@ -25,5 +31,27 @@ class Price extends Model
     public function property(): BelongsTo
     {
         return $this->belongsTo(Property::class);
+    }
+
+    /**
+     * Determen if price is active
+     */
+    public function isActive() :bool
+    {
+        if (Carbon::parse($this->from) <= now() && Carbon::parse($this->to >= now())) return true;
+
+        return false;
+    }
+
+    public function getIsActiveAttribute() :bool
+    {
+        return $this->isActive();
+    }
+
+    public function scopeActive(Builder $query) :Builder
+    {
+        return $query->where('to', '<=', now())
+                ->where('from', '>=', now());
+
     }
 }
